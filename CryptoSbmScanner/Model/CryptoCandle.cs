14ce55e62770e48eb461ce735535eaa5d7e13f88
Marius <59998287+M22arius494@users.noreply.github.com>
2023-04-17 08:51:13 +0200

    DumpCandles methode afgezonderd

--- CryptoSbmScanner/Model/CryptoCandle.cs
+++ CryptoSbmScanner/Model/CryptoCandle.cs
@@ -21,3 +21,42 @@ public class CryptoCandle : IQuote
 
     public string ExtraText; // beetje quick en dirty
 }
+
+//
+// Voor een toekomstige Helperclass wellicht? (naar een StringStream oid)
+//
+//public void DumpSymbol()
+//{
+//    //Ter debug van een hardnekig probleem met het tonen van de signal
+//    var csv = new StringBuilder();
+//    var newLine = string.Format("{0};{1};{2};{3};{4};{5};{6};{7}", "OpenTime", "IntervalId", "Open", "High", "Low", "Close", "Volume");
+//    csv.AppendLine(newLine);
+
+//    Monitor.Enter(history);
+//    try
+//    {
+//        for (int i = 0; i < history.Count; i++)
+//        {
+//            CryptoCandle candle = history[i];
+
+//            newLine = string.Format("{0};{1};{2};{3};{4};{5};{6}",
+//            candle.Date.ToString(),
+//            candle.IntervalId.ToString(),
+//            candle.Open.ToString(),
+//            candle.High.ToString(),
+//            candle.Low.ToString(),
+//            candle.Close.ToString(),
+//            candle.Volume.ToString());
+
+//            csv.AppendLine(newLine);
+//        }
+//    }
+//    finally
+//    {
+//        Monitor.Exit(history);
+//    }
+//    string filename = System.IO.Path.GetDirectoryName((System.Reflection.Assembly.GetEntryAssembly().Location));
+//    filename = filename + @"\data\" + Symbol.Exchange.Name + @"\Candles\" + Interval.Name + @"\";
+//    System.IO.Directory.CreateDirectory(filename);
+//    System.IO.File.WriteAllText(filename + Symbol.Name + "-" + Interval.Name + ".csv", csv.ToString());
+//}
