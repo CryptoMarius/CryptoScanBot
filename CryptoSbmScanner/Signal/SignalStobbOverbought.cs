83c447ce8ebb831a6e3c4eacc96e51e68186a9e7
Marius <59998287+M22arius494@users.noreply.github.com>
2023-07-22 16:48:42 +0200

    Refactoring

--- CryptoSbmScanner/Signal/SignalStobbOverbought.cs
+++ CryptoSbmScanner/Signal/SignalStobbOverbought.cs
@@ -43,7 +43,7 @@ public class SignalStobbOverbought : SignalSbmBaseOverbought
         // Controle op de ma-lijnen
         if (GlobalData.Settings.Signal.StobIncludeSoftSbm)
         {
-            if (!IsSbmConditionsOverbought(CandleLast, false))
+            if (!CandleLast.IsSbmConditionsOverbought(false))
             {
                 response = "geen sbm condities";
                 return false;
