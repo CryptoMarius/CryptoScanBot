e687c2fbea1b52f5dcd5a9fcd4651db4817a6f36
Marius <59998287+M22arius494@users.noreply.github.com>
2023-07-22 16:16:53 +0200

    Een eerste aanvang met een trading dashboard (graphs)

--- CryptoSbmScanner/UserControl/DashBoardControl.Designer.cs
+++ CryptoSbmScanner/UserControl/DashBoardControl.Designer.cs
@@ -29,67 +29,143 @@ partial class DashBoardControl
     private void InitializeComponent()
     {
         button1 = new Button();
-        checkBox1 = new CheckBox();
-        listBox1 = new ListBox();
         label1 = new Label();
         label2 = new Label();
+        label5 = new Label();
+        label6 = new Label();
+        label7 = new Label();
+        labelReturned = new Label();
+        labelCommission = new Label();
+        labelNettoPnlValue = new Label();
+        labelInvested = new Label();
+        labelPositions = new Label();
+        label8 = new Label();
         SuspendLayout();
         // 
         // button1
         // 
-        button1.Location = new Point(95, 105);
+        button1.Location = new Point(537, 3);
         button1.Name = "button1";
         button1.Size = new Size(75, 23);
         button1.TabIndex = 0;
-        button1.Text = "button1";
+        button1.Text = "Ververs";
         button1.UseVisualStyleBackColor = true;
-        // 
-        // checkBox1
-        // 
-        checkBox1.AutoSize = true;
-        checkBox1.Location = new Point(124, 152);
-        checkBox1.Name = "checkBox1";
-        checkBox1.Size = new Size(83, 19);
-        checkBox1.TabIndex = 1;
-        checkBox1.Text = "checkBox1";
-        checkBox1.UseVisualStyleBackColor = true;
-        // 
-        // listBox1
-        // 
-        listBox1.FormattingEnabled = true;
-        listBox1.ItemHeight = 15;
-        listBox1.Location = new Point(144, 187);
-        listBox1.Name = "listBox1";
-        listBox1.Size = new Size(116, 169);
-        listBox1.TabIndex = 2;
+        button1.Click += button1_Click;
         // 
         // label1
         // 
         label1.AutoSize = true;
-        label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-        label1.Location = new Point(21, 25);
+        label1.Location = new Point(46, 83);
         label1.Name = "label1";
-        label1.Size = new Size(267, 15);
-        label1.TabIndex = 3;
-        label1.Text = "Hier komt het toekomstige dashboard te staan";
+        label1.Size = new Size(75, 15);
+        label1.TabIndex = 1;
+        label1.Text = "Geinvesteerd";
         // 
         // label2
         // 
         label2.AutoSize = true;
-        label2.Location = new Point(55, 70);
+        label2.Location = new Point(27, 30);
         label2.Name = "label2";
-        label2.Size = new Size(129, 15);
-        label2.TabIndex = 4;
-        label2.Text = "Wat test componentjes";
+        label2.Size = new Size(71, 15);
+        label2.TabIndex = 2;
+        label2.Text = "Openstaand";
+        // 
+        // label5
+        // 
+        label5.AutoSize = true;
+        label5.Location = new Point(46, 160);
+        label5.Name = "label5";
+        label5.Size = new Size(62, 15);
+        label5.TabIndex = 5;
+        label5.Text = "Netto PNL";
+        // 
+        // label6
+        // 
+        label6.AutoSize = true;
+        label6.Location = new Point(46, 135);
+        label6.Name = "label6";
+        label6.Size = new Size(66, 15);
+        label6.TabIndex = 6;
+        label6.Text = "Commissie";
+        // 
+        // label7
+        // 
+        label7.AutoSize = true;
+        label7.Location = new Point(46, 108);
+        label7.Name = "label7";
+        label7.Size = new Size(83, 15);
+        label7.TabIndex = 7;
+        label7.Text = "Geretourneerd";
+        // 
+        // labelReturned
+        // 
+        labelReturned.Location = new Point(153, 108);
+        labelReturned.Name = "labelReturned";
+        labelReturned.Size = new Size(83, 15);
+        labelReturned.TabIndex = 12;
+        labelReturned.Text = "Geretourneerd";
+        labelReturned.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // labelCommission
+        // 
+        labelCommission.Location = new Point(170, 135);
+        labelCommission.Name = "labelCommission";
+        labelCommission.Size = new Size(66, 15);
+        labelCommission.TabIndex = 11;
+        labelCommission.Text = "Commissie";
+        labelCommission.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // labelNettoPnlValue
+        // 
+        labelNettoPnlValue.Location = new Point(174, 160);
+        labelNettoPnlValue.Name = "labelNettoPnlValue";
+        labelNettoPnlValue.Size = new Size(62, 15);
+        labelNettoPnlValue.TabIndex = 10;
+        labelNettoPnlValue.Text = "Netto PNL";
+        labelNettoPnlValue.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // labelInvested
+        // 
+        labelInvested.Location = new Point(161, 83);
+        labelInvested.Name = "labelInvested";
+        labelInvested.Size = new Size(75, 15);
+        labelInvested.TabIndex = 8;
+        labelInvested.Text = "Geinvesteerd";
+        labelInvested.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // labelPositions
+        // 
+        labelPositions.Location = new Point(161, 58);
+        labelPositions.Name = "labelPositions";
+        labelPositions.Size = new Size(75, 15);
+        labelPositions.TabIndex = 14;
+        labelPositions.Text = "Geinvesteerd";
+        labelPositions.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // label8
+        // 
+        label8.AutoSize = true;
+        label8.Location = new Point(46, 58);
+        label8.Name = "label8";
+        label8.Size = new Size(120, 15);
+        label8.TabIndex = 13;
+        label8.Text = "Openstaande posities";
         // 
         // DashBoardControl
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
+        Controls.Add(labelPositions);
+        Controls.Add(label8);
+        Controls.Add(labelReturned);
+        Controls.Add(labelCommission);
+        Controls.Add(labelNettoPnlValue);
+        Controls.Add(labelInvested);
+        Controls.Add(label7);
+        Controls.Add(label6);
+        Controls.Add(label5);
         Controls.Add(label2);
         Controls.Add(label1);
-        Controls.Add(listBox1);
-        Controls.Add(checkBox1);
         Controls.Add(button1);
         Name = "DashBoardControl";
         Size = new Size(615, 403);
@@ -100,8 +176,15 @@ partial class DashBoardControl
     #endregion
 
     private Button button1;
-    private CheckBox checkBox1;
-    private ListBox listBox1;
     private Label label1;
     private Label label2;
+    private Label label5;
+    private Label label6;
+    private Label label7;
+    private Label labelReturned;
+    private Label labelCommission;
+    private Label labelNettoPnlValue;
+    private Label labelInvested;
+    private Label labelPositions;
+    private Label label8;
 }
--- CryptoSbmScanner/UserControl/DashBoardControl.cs
+++ CryptoSbmScanner/UserControl/DashBoardControl.cs
@@ -1,24 +1,432 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Text;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using CryptoSbmScanner.Context;
+using CryptoSbmScanner.Enums;
+
+using Dapper;
 
 namespace CryptoSbmScanner;
 
+// Charts:
+// https://stackoverflow.com/questions/10622674/chart-creating-dynamically-in-net-c-sharp
+// https://stackoverflow.com/questions/335061/add-dynamic-charts-using-asp-net-chart-control-c-sharp
+
+
 public partial class DashBoardControl : UserControl
 {
+    public class QueryPositionData
+    {
+        public DateTime CloseTime { get; set; }
+        public string Quote { get; set; }
+        public CryptoOrderStatus Status { get; set; }
+
+        // Aantal per dag
+        public int Positions { get; set; }
+
+        // Wat totalen
+        public decimal Invested { get; set; }
+        public decimal Returned { get; set; }
+        public decimal Commission { get; set; }
+        public decimal TotalProfit { get; set; }
+
+        // De doorlooptijd in minuten
+        public decimal MinMin { get; set; }
+        public decimal AvgMin { get; set; }
+        public decimal MaxMin { get; set; }
+
+        // De winst percentages
+        public decimal MinPerc { get; set; }
+        public decimal AvgPerc { get; set; }
+        public decimal MaxPerc { get; set; }
+    }
+
+    public class QueryTradeData
+    {
+        public DateTime TradeTime { get; set; }
+        public CryptoOrderSide Side { get; set; }
+        public string Quote { get; set; }
+
+        // Wat totalen
+        public decimal Value { get; set; }
+    }
+
+
+    private readonly int OffsetX = 25;
+    private readonly int OffsetY = 200;
+    private readonly int GraphWidth = 500;
+    private readonly int GraphHeight = 300;
+
+    private QueryPositionData OpenData = new();
+    private List<QueryTradeData> QueryTradeDataList = new();
+    private List<QueryPositionData> QueryPositionDataList = new();
+
+    private Chart ChartPositionsPerDay;
+    private Chart ChartProfitsPerDay;
+    private Chart ChartProfitPercentagePerDay;
+    private Chart ChartInvestedReturnedPerDay;
+
+
     public DashBoardControl()
     {
         InitializeComponent();
     }
 
+    private void GetQueryTradeData()
+    {
+        // dit is de query die per dag het een en ander aan info ophaalt
+        StringBuilder builder = new();
+        builder.AppendLine("select date(position.CloseTime) as CloseTime, symbol.quote, position.Status, count(position.id) as Positions,");
+        builder.AppendLine("round(MIN(ROUND((JULIANDAY(position.CloseTime) - JULIANDAY(position.CreateTime)) * 86400 / 60)), 2) AS MinMin, -- in minutes");
+        builder.AppendLine("round(AVG(ROUND((JULIANDAY(position.CloseTime) - JULIANDAY(position.CreateTime)) * 86400 / 60)), 2) AS AvgMin, -- in minutes");
+        builder.AppendLine("round(MAX(ROUND((JULIANDAY(position.CloseTime) - JULIANDAY(position.CreateTime)) * 86400 / 60)), 2) AS MaxMin, -- in minutes");
+        builder.AppendLine("sum(position.Invested) as Invested,");
+        builder.AppendLine("sum(position.Returned) as Returned,");
+        builder.AppendLine("sum(position.Commission) as Commission,");
+        builder.AppendLine("sum(position.Profit) as TotalProfit,  --= sum(position.Returned - position.Invested - position.Commission) as Open,");
+        builder.AppendLine("--100 * sum(position.Profit) / sum(position.Invested) as Average");
+        builder.AppendLine("round(min(round(position.percentage, 2)), 2) as MinPerc,");
+        builder.AppendLine("round(avg(round(position.percentage, 2)), 2) as AvgPerc,");
+        builder.AppendLine("round(max(round(position.percentage, 2)), 2) as MaxPerc");
+        builder.AppendLine("from position");
+        builder.AppendLine("inner join symbol on position.symbolid = symbol.id");
+        builder.AppendLine("--where position.status = 2");
+        builder.AppendLine("where symbol.quote = 'USDT'");
+        builder.AppendLine("group by date(position.CloseTime), position.Status, symbol.quote");
+        builder.AppendLine("order by date(position.CloseTime) asc, position.Status, symbol.quote");
 
-    // Charts:
-    // https://stackoverflow.com/questions/10622674/chart-creating-dynamically-in-net-c-sharp
-    // https://stackoverflow.com/questions/335061/add-dynamic-charts-using-asp-net-chart-control-c-sharp
+        using CryptoDatabase databaseThread = new();
+        databaseThread.Open();
+
+        // Experiment #1 is een chart met per datum het aantal gesloten posities
+        // De 1e kolom is het aantal nog openstaande posities, die moeten we nog ergens onderbrengen
+        QueryPositionDataList.Clear();
+        foreach (QueryPositionData data in databaseThread.Connection.Query<QueryPositionData>(builder.ToString()))
+        {
+            if (data.CloseTime.Date > new DateTime(2000, 01, 01))
+                QueryPositionDataList.Add(data);
+            else
+                OpenData = data; // het restant
+        }
+    }
+
+    private void GetQueryPositionData()
+    {
+        // Mhhhh, dat zou eigenlijk via de trades moeten lopen
+        // dit is de query die per dag het een en ander aan info ophaalt
+        StringBuilder builder = new();
+        builder.AppendLine("select");
+        builder.AppendLine("  date(trade.TradeTime) as TradeTime,");
+        builder.AppendLine("  trade.Side,");
+        builder.AppendLine("  symbol.quote,");
+        builder.AppendLine("  sum(trade.QuoteQuantity) as Value");
+        builder.AppendLine("from trade");
+        builder.AppendLine("inner join symbol on trade.symbolid = symbol.id");
+        builder.AppendLine("where symbol.quote = 'USDT'");
+        builder.AppendLine("group by date(trade.TradeTime), trade.Side,symbol.quote");
+        builder.AppendLine("order by date(trade.TradeTime) asc, trade.Side,symbol.quote");
+
+        using CryptoDatabase databaseThread = new();
+        databaseThread.Open();
+
+        QueryTradeDataList.Clear();
+        foreach (QueryTradeData data in databaseThread.Connection.Query<QueryTradeData>(builder.ToString()))
+        {
+            if (data.TradeTime.Date > new DateTime(2000, 01, 01))
+            {
+                QueryTradeDataList.Add(data);
+            }
+        }
+    }
+
+    private Chart CreateChart(string title, int x, int y)
+    {
+        Chart chart = new Chart();
+        //chart.Title = "Title of the Chart";
+        //chart.DisplayTitle = true;
+        //chart.TabIndex = 0;
+        //chart.Name = "chartname";
+        //chart.Text = "charttext";
+        chart.Titles.Add(title).ForeColor = Color.White;
+        chart.BackColor = Color.Black;
+        chart.Location = new(x, y);
+        chart.Size = new Size(GraphWidth, GraphHeight);
+        //chart.Dock = DockStyle.Fill; // right
+        //chart1.ChartAreas.Clear();
+        return chart;
+    }
+
+    private static ChartArea CreateChartArea(string axisYFormat)
+    {
+        ChartArea chartArea = new();
+        //chartArea1.Name = "ChartAreaName";
+        chartArea.BackColor = Color.Black;
+
+        chartArea.AxisX.LabelStyle.Format = "dd";
+        chartArea.AxisX.LineColor = Color.Gray;
+        chartArea.AxisX.MinorGrid.LineColor = Color.Gray;
+        chartArea.AxisX.MajorGrid.LineColor = Color.Gray;
+        chartArea.AxisX.LabelStyle.ForeColor = Color.Gray;
+        chartArea.AxisX.LabelStyle.Enabled = true;
+
+        //chartArea.AxisX.TitleAlignment = StringAlignment.Far;
+        //chartArea.AxisX.TextOrientation = TextOrientation.Horizontal;
+        //chartArea.AxisX.Title = "Datum";
+        //chartArea.AxisX.TitleForeColor = Color.Gray;
+        //chartArea.AxisX.TitleFont = new Font("Tahoma", 7, FontStyle.Bold);
+
+        chartArea.AxisY.LabelStyle.Format = axisYFormat;
+        chartArea.AxisY.LineColor = Color.Gray;
+        chartArea.AxisY.MinorGrid.LineColor = Color.Gray;
+        chartArea.AxisY.MajorGrid.LineColor = Color.Gray;
+        chartArea.AxisY.LabelStyle.ForeColor = Color.Gray;
+        chartArea.AxisY.LabelStyle.Enabled = true;
+
+        //chartArea.AxisY.TitleAlignment = StringAlignment.Far;
+        //chartArea.AxisY.TextOrientation = TextOrientation.Horizontal;
+        //chartArea.AxisY.Title = "Aantal";
+        //chartArea.AxisY.TitleForeColor = Color.Gray;
+        //chartArea.AxisY.TitleFont = new Font("Tahoma", 7, FontStyle.Bold);
+
+        return chartArea;
+    }
+
+    private void DoChartPositionsPerDay(int x, int y)
+    {
+        if (ChartPositionsPerDay == null)
+        {
+            ChartPositionsPerDay = CreateChart("Aantal gesloten posities per dag", x, y);
+            Controls.Add(ChartPositionsPerDay);
+
+            ChartArea chartArea = CreateChartArea("N0");
+            ChartPositionsPerDay.ChartAreas.Add(chartArea);
+
+
+            //chart1.Legends.Clear();
+            //chart1.Legends.Add(legend1);
+            //Legend legend1 = chart1.Legends.Add("legenda");
+            //legend1.BackColor = Color.Black;
+            //legend1.ForeColor = Color.White;
+        }
+
+        ChartPositionsPerDay.Series.Clear();
+        var series1 = new Series
+        {
+            Name = "Posities",
+            Color = Color.Green,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Column,
+        };
+        ChartPositionsPerDay.Series.Add(series1);
+
+
+        // Experiment #1 is een chart met per datum het aantal gesloten posities
+        // De 1e kolom is het aantal nog openstaande posities, die moeten we nog ergens onderbrengen
+        foreach (QueryPositionData data in QueryPositionDataList)
+        {
+            if (data.CloseTime.Date > new DateTime(2000, 01, 01))
+                series1.Points.AddXY(data.CloseTime.Date, data.Positions);
+
+            //DataPoint dataPoint = new DataPoint() { AxisLabel = "series", YValues = new double[] { dataPoint } };
+            //series1.Points.Add(dataPoint);
+        }
+        ChartPositionsPerDay.Invalidate();
+    }
+
+    private void DoChartProfitsPerDay(int x, int y)
+    {
+        if (ChartProfitsPerDay == null)
+        {
+            ChartProfitsPerDay = CreateChart("Behaalde winst bedrag per dag", x, y);
+            Controls.Add(ChartProfitsPerDay);
+
+            ChartArea chartArea = CreateChartArea("N2");
+            ChartProfitsPerDay.ChartAreas.Add(chartArea);
+
+            //chart1.Legends.Clear();
+            //chart1.Legends.Add(legend1);
+            //Legend legend1 = chart1.Legends.Add("legenda");
+            //legend1.BackColor = Color.Black;
+            //legend1.ForeColor = Color.White;
+        }
+        ChartProfitsPerDay.Series.Clear();
+
+
+        var series1 = new Series
+        {
+            Name = "Winst",
+            Color = Color.Green,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Column,
+        };
+        ChartProfitsPerDay.Series.Add(series1);
+
+
+        foreach (QueryPositionData data in QueryPositionDataList)
+        {
+            if (data.CloseTime.Date > new DateTime(2000, 01, 01))
+                series1.Points.AddXY(data.CloseTime.Date, data.TotalProfit);
+        }
+        ChartProfitsPerDay.Invalidate();
+    }
+
+
+    private void DoChartProfitPercentagePerDay(int x, int y)
+    {
+        // AVG profit per trade
+        if (ChartProfitPercentagePerDay == null)
+        {
+            ChartProfitPercentagePerDay = CreateChart("Minimale, maximale en gemiddelde winst percentage per dag", x, y);
+            Controls.Add(ChartProfitPercentagePerDay);
+
+            ChartArea chartArea = CreateChartArea("N2");
+            ChartProfitPercentagePerDay.ChartAreas.Add(chartArea);
+
+
+            ChartProfitPercentagePerDay.Legends.Clear();
+            //chart1.Legends.Add(legend1);
+            Legend legend1 = ChartProfitPercentagePerDay.Legends.Add("legenda");
+            legend1.BackColor = Color.Black;
+            legend1.ForeColor = Color.White;
+
+        }
+        ChartProfitPercentagePerDay.Series.Clear();
+
+
+        var series1 = new Series
+        {
+            Name = "Minimaal",
+            Color = Color.Red,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Line,
+        };
+        ChartProfitPercentagePerDay.Series.Add(series1);
+
+        var series2 = new Series
+        {
+            Name = "Gemiddeld",
+            Color = Color.Orange,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Line,
+        };
+        ChartProfitPercentagePerDay.Series.Add(series2);
+
+        var series3 = new Series
+        {
+            Name = "Maximaal",
+            Color = Color.Green,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Line,
+        };
+        ChartProfitPercentagePerDay.Series.Add(series3);
+
+        // Experiment #2 is een chart met per datum het aantal gesloten posities
+        // De 1e kolom is het aantal nog openstaande posities, die moeten we nog ergens onderbrengen
+        foreach (QueryPositionData data in QueryPositionDataList)
+        {
+            if (data.CloseTime.Date > new DateTime(2000, 01, 01))
+            {
+                series1.Points.AddXY(data.CloseTime.Date, data.MinPerc - 100);
+                series2.Points.AddXY(data.CloseTime.Date, data.AvgPerc - 100);
+                series3.Points.AddXY(data.CloseTime.Date, data.MaxPerc - 100);
+            }
+        }
+        ChartProfitPercentagePerDay.Invalidate();
+    }
+
+    private void DoChartInvestedReturnedPerDay(int x, int y)
+    {
+        if (ChartInvestedReturnedPerDay == null)
+        {
+            ChartInvestedReturnedPerDay = CreateChart("Geinvesteerde en geretourneede bedragen per dag", x, y);
+            Controls.Add(ChartInvestedReturnedPerDay);
+
+            ChartArea chartArea = CreateChartArea("N2");
+            ChartInvestedReturnedPerDay.ChartAreas.Add(chartArea);
+
+            ChartInvestedReturnedPerDay.Legends.Clear();
+            //chart1.Legends.Add(legend1);
+            var legend1 = ChartInvestedReturnedPerDay.Legends.Add("legenda");
+            legend1.BackColor = Color.Black;
+            legend1.ForeColor = Color.White;
+        }
+        ChartInvestedReturnedPerDay.Series.Clear();
+
+
+        var series1 = new Series
+        {
+            Name = "Geinvesteerd",
+            Color = Color.Red,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Line,
+        };
+        ChartInvestedReturnedPerDay.Series.Add(series1);
+
+        var series2 = new Series
+        {
+            Name = "Geretourneerd",
+            Color = Color.Green,
+            IsVisibleInLegend = true,
+            IsXValueIndexed = true,
+            //ChartType = SeriesChartType.Bar
+            ChartType = SeriesChartType.Line,
+        };
+        ChartInvestedReturnedPerDay.Series.Add(series2);
+
+
+        foreach (QueryTradeData data in QueryTradeDataList)
+        {
+            if (data.TradeTime.Date > new DateTime(2000, 01, 01))
+            {
+                if (data.Side == CryptoOrderSide.Buy)
+                    series1.Points.AddXY(data.TradeTime.Date, data.Value);
+                else
+                    series2.Points.AddXY(data.TradeTime.Date, data.Value);
+            }
+        }
+
+        ChartInvestedReturnedPerDay.Invalidate();
+    }
+
+    private void CreateChart()
+    {
+        // Diverse statistieken van de posities per dag (via position tabel)
+        // +Investeringen en returnments per dag (via trade tabel)
+
+        GetQueryPositionData();
+        GetQueryTradeData();
+
+        DoChartPositionsPerDay(OffsetX, OffsetY);
+        DoChartProfitPercentagePerDay(OffsetX + GraphWidth + 25, OffsetY);
+
+        DoChartProfitsPerDay(OffsetX, OffsetY + GraphHeight + 25);
+        DoChartInvestedReturnedPerDay(OffsetX + GraphWidth + 25, OffsetY + GraphHeight + 25);
+
+        // En de lopende posities
+        labelPositions.Text = OpenData.Positions.ToString();
+        labelInvested.Text = OpenData.Invested.ToString();
+        labelReturned.Text = OpenData.Returned.ToString();
+        labelCommission.Text = OpenData.Commission.ToString();
+        labelNettoPnlValue.Text = (OpenData.Invested - OpenData.Returned - OpenData.Commission).ToString();
+            //OpenData.TotalProfit.ToString();
+
+        return;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        CreateChart();
+    }
 }
