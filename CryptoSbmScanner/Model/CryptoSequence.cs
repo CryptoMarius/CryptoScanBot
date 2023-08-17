f5fa4088b36a2bff9526162978419e480534c734
Marius <59998287+M22arius494@users.noreply.github.com>
2023-08-17 12:05:10 +0200

    Nieuwe tabel voor het genereren van ID's

---
+++ CryptoSbmScanner/Model/CryptoSequence.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoSbmScanner.Model;
+
+using Dapper.Contrib.Extensions;
+
+[Table("Sequence")]
+public class CryptoSequence
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
