975b93bcb189791401bea862447cbdf7a3e12bde
Marius <59998287+M22arius494@users.noreply.github.com>
2023-08-23 19:42:46 +0200

    de DynamicPercentage verwijderd

--- CryptoSbmScanner/Enums/CryptoSellMethod.cs
+++ CryptoSbmScanner/Enums/CryptoSellMethod.cs
@@ -3,6 +3,6 @@
 public enum CryptoSellMethod
 {
     FixedPercentage, // Het opgegeven vaste percentage
-    DynamicPercentage, // Het percentage adhv BB breedte
+    //DynamicPercentage, // Het percentage adhv BB breedte
     TrailViaKcPsar // stop limit sell op de bovenste KC/PSAR
 }
