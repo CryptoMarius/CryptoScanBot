f5d7c910d58de7c7cc69a81d1651c62dfebd3c14
Marius <59998287+M22arius494@users.noreply.github.com>
2023-09-24 15:57:26 +0200

    Naam van een enum aangepast

---
+++ CryptoSbmScanner/Enums/CryptoStepInMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoSbmScanner.Enums;
+
+public enum CryptoStepInMethod
+{
+    AfterNextSignal, // Stap op een melding in (rekening houdende met cooldown en percentage indien DCA)
+    FixedPercentage, // Stap in op een gefixeerde percentage (rekening houdende met cooldown en percentage indien DCA)
+    TrailViaKcPsar // Stap in na een signaal of fixed percentage en ga dan boven (of beneden) trailen
+}
