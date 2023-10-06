eff96611d3225194b1f3403156ede2ad4b152282
Marius <59998287+M22arius494@users.noreply.github.com>
2023-10-06 11:06:43 +0200

    Alle veel gebruikte commando's in 1 bestand

---
+++ CryptoSbmScanner/Intern/Commands.cs
@@ -0,0 +1,266 @@
+﻿using System.Text;
+
+using CryptoSbmScanner.Context;
+using CryptoSbmScanner.Enums;
+using CryptoSbmScanner.Model;
+
+namespace CryptoSbmScanner.Intern;
+
+public enum Command
+{
+    ActivateTradingApp,
+    ActivateTradingviewIntern,
+    ActivateTradingviewExtern,
+    ShowTrendInformation,
+    ExcelSymbolInformation,
+    ExcelExchangeInformation,
+    ExcelPositionInformation
+}
+
+//class CommandEventArgs : EventArgs
+//{
+//    public Command Command { get; private set; }
+
+//    public CommandEventArgs(Command command)
+//    {
+//        Command = command;
+//    }
+//}
+
+
+
+public class Commands
+{
+    public Commands()
+    {
+    }
+
+
+    //public static void ExecuteCommandCommandViaTag(object sender, EventArgs e)
+    //{
+    //    if (sender is ToolStripMenuItem item && e is CommandEventArgs cmd)
+    //    {
+    //        var (succes, symbol, interval) = GetAttributesFromSender(sender);
+    //        if (succes)
+    //        {
+    //            switch (cmd.Command)
+    //            {
+    //                case Command.ActivateTradingApp:
+    //                    LinkTools.ActivateTradingApp(GlobalData.Settings.General.TradingApp, symbol, interval, CryptoExternalUrlType.External);
+    //                    break;
+    //                case Command.ActivateTradingviewIntern:
+    //                    LinkTools.ActivateTradingApp(CryptoTradingApp.TradingView, symbol, interval, CryptoExternalUrlType.Internal);
+    //                    break;
+    //                case Command.ActivateTradingviewExtern:
+    //                    LinkTools.ActivateTradingApp(CryptoTradingApp.TradingView, symbol, interval, CryptoExternalUrlType.External);
+    //                    break;
+    //                case Command.ShowTrendInformation:
+    //                    ShowTrendInformation(symbol);
+    //                    break;
+    //                case Command.ExcelSymbolInformation:
+    //                    Task.Run(() => { new Excel.ExcelSymbolDump().ExportToExcell(symbol); });
+    //                    break;
+    //                case Command.ExcelExchangeInformation:
+    //                    // Die valt qua parameters buiten de boot
+    //                    Task.Run(() => { new Excel.ExcelExchangeDump().ExportToExcell(GlobalData.Settings.General.Exchange); });
+    //                    break;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+//    private static async void CommandPositionExcelDumpExecute(object sender, EventArgs e)
+//    {
+//#if TRADEBOT
+//        if (sender is ListView listview)
+//        {
+//            if (listview.SelectedItems.Count > 0)
+//            {
+//                ListViewItem item = listview.SelectedItems[0];
+//                CryptoPosition position = (CryptoPosition)item.Tag;
+
+//                using CryptoDatabase databaseThread = new();
+//                databaseThread.Open();
+//                PositionTools.LoadPosition(databaseThread, position);
+
+//                await PositionTools.LoadTradesfromDatabaseAndExchange(databaseThread, position);
+//                PositionTools.CalculatePositionResultsViaTrades(databaseThread, position);
+
+//                //Task.Run(() => { Invoke(new Action(() => { new PositionDumpDebug().ExportToExcell(position); })); });
+//                _ = Task.Run(() => { new Excel.ExcelPositionDump().ExportToExcel(position); });
+//            }
+//        }
+//#endif
+//    }
+
+    private static void ShowTrendInformation(CryptoSymbol symbol)
+    {
+        StringBuilder log = new();
+        log.AppendLine("Trend " + symbol.Name);
+
+        GlobalData.AddTextToLogTab("");
+        GlobalData.AddTextToLogTab("Trend " + symbol.Name);
+
+        long percentageSum = 0;
+        long maxPercentageSum = 0;
+        foreach (CryptoInterval interval in GlobalData.IntervalList)
+        {
+            log.AppendLine("");
+            log.AppendLine("----");
+            log.AppendLine("Interval " + interval.Name);
+
+            // Wat is het maximale som (voor de eindberekening)
+            maxPercentageSum += interval.Duration;
+
+            TrendIndicator trendIndicatorClass = new(symbol, interval)
+            {
+                Log = log
+            };
+            CryptoTrendIndicator trendIndicator = trendIndicatorClass.CalculateTrend();
+            if (trendIndicator == CryptoTrendIndicator.trendBullish)
+                percentageSum += interval.Duration;
+            else if (trendIndicator == CryptoTrendIndicator.trendBearish)
+                percentageSum -= interval.Duration;
+
+
+            // Ahh, dat gaat niet naar een tabel (zoals ik eerst dacht)
+            //CryptoSymbolInterval symbolInterval = signal.Symbol.GetSymbolInterval(interval.IntervalPeriod);
+            //symbolInterval.TrendIndicator = trendIndicator;
+            //symbolInterval.TrendInfoDate = DateTime.UtcNow;
+
+            string s = "";
+            if (trendIndicator == CryptoTrendIndicator.trendBullish)
+                s = string.Format("{0} {1}, trend=bullish", symbol.Name, interval.IntervalPeriod);
+            else if (trendIndicator == CryptoTrendIndicator.trendBearish)
+                s = string.Format("{0} {1}, trend=bearish", symbol.Name, interval.IntervalPeriod);
+            else
+                s = string.Format("{0} {1}, trend=sideway's", symbol.Name, interval.IntervalPeriod);
+            GlobalData.AddTextToLogTab(s);
+            log.AppendLine(s);
+        }
+
+        if (maxPercentageSum > 0)
+        {
+            decimal trendPercentage = 100 * (decimal)percentageSum / (decimal)maxPercentageSum;
+            string t = string.Format("{0} {1:N2}", symbol.Name, trendPercentage);
+            GlobalData.AddTextToLogTab(t);
+            log.AppendLine(t);
+        }
+
+
+
+        //Laad de gecachte (langere historie, minder overhad)
+        string filename = GlobalData.GetBaseDir() + "Trend information.txt";
+        File.WriteAllText(filename, log.ToString());
+    }
+
+
+
+    public static (bool succes, CryptoSymbol symbol, CryptoInterval interval, CryptoPosition position) GetAttributesFromSender(object sender)
+    {
+        // Vanuit de signalen of de open of gesloten posities
+        if (sender is ListViewHeaderContext listview && listview.SelectedItems.Count > 0)
+        {
+            ListViewItem listviewItem = listview.SelectedItems[0];
+            if (listviewItem.Tag is CryptoSignal signal)
+                return (true, signal.Symbol, signal.Interval, null);
+            else if (listviewItem.Tag is CryptoPosition position)
+                return (true, position.Symbol, position.Interval, position);
+        }
+        else
+        // Vanuit de lijst met symbols
+        if (sender is ListBox listbox && listbox.SelectedItems.Count > 0)
+        {
+            if (GlobalData.ExchangeListName.TryGetValue(GlobalData.Settings.General.ExchangeName, out Model.CryptoExchange exchange))
+            {
+                // Neem de door de gebruiker geselecteerde coin
+                string symbolName = listbox.Text.ToString();
+                if (!string.IsNullOrEmpty(symbolName))
+                {
+                    if (exchange.SymbolListName.TryGetValue(symbolName, out CryptoSymbol symbol))
+                    {
+                        if (symbol.QuoteData.FetchCandles && symbol.Status == 1)
+                            return (true, symbol, GlobalData.IntervalList[0], null);
+                    }
+                }
+
+            }
+        }
+
+        return (false, null, null, null);
+    }
+
+
+    public static async void ExecuteSomethingViaTag(object sender, Command cmd)
+    {
+        var (succes, symbol, interval, position) = GetAttributesFromSender(sender);
+        if (succes)
+        {
+            switch (cmd)
+            {
+                case Command.ActivateTradingApp:
+                    LinkTools.ActivateTradingApp(GlobalData.Settings.General.TradingApp, symbol, interval, CryptoExternalUrlType.External);
+                    break;
+                case Command.ActivateTradingviewIntern:
+                    LinkTools.ActivateTradingApp(CryptoTradingApp.TradingView, symbol, interval, CryptoExternalUrlType.Internal);
+                    break;
+                case Command.ActivateTradingviewExtern:
+                    LinkTools.ActivateTradingApp(CryptoTradingApp.TradingView, symbol, interval, CryptoExternalUrlType.External);
+                    break;
+                case Command.ShowTrendInformation:
+                    ShowTrendInformation(symbol);
+                    break;
+                case Command.ExcelSymbolInformation:
+                    _ = Task.Run(() => { new Excel.ExcelSymbolDump().ExportToExcel(symbol); });
+                    break;
+                case Command.ExcelExchangeInformation:
+                    // Die valt qua parameters buiten de boot
+                    _ = Task.Run(() => { new Excel.ExcelExchangeDump().ExportToExcel(GlobalData.Settings.General.Exchange); });
+                    break;
+#if TRADEBOT
+                case Command.ExcelPositionInformation:
+                    using (CryptoDatabase databaseThread = new())
+                    {
+                        databaseThread.Open();
+                        PositionTools.LoadPosition(databaseThread, position);
+
+                        await PositionTools.LoadTradesfromDatabaseAndExchange(databaseThread, position);
+                        PositionTools.CalculatePositionResultsViaTrades(databaseThread, position);
+
+                        _ = Task.Run(() => { new Excel.ExcelPositionDump().ExportToExcel(position); });
+                    }
+                    break;
+#endif
+            }
+        }
+    }
+
+    public static void ExecuteCommandCommandViaTag(object sender, EventArgs e)
+    {
+        // Een poging om de meest gebruikte menu items te centraliseren
+
+        if (sender is ToolStripMenuItem tsitem && tsitem.Tag is Command cmd)
+        {
+            if (sender is ToolStripMenuItem item && item.Owner is ContextMenuStrip strip)
+            {
+                ExecuteSomethingViaTag(strip.SourceControl, cmd);
+            }
+        }
+        else if (sender is ListView listview)
+        {
+            if (listview.Tag is Command cmd2)
+            {
+                ExecuteSomethingViaTag(listview, cmd2);
+            }
+        }
+        else if (sender is ListBox listbox)
+        {
+            if (listbox.Tag is Command cmd3)
+            {
+                ExecuteSomethingViaTag(listbox, cmd3);
+            }
+        }
+    }
+
+}
