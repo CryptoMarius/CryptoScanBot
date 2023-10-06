a5c6883d61c756e1f2ff02e433234ca0953a9f8e
Marius <59998287+M22arius494@users.noreply.github.com>
2023-10-06 12:05:05 +0200

    Ongebruikte code verwijderd

--- CryptoSbmScanner/ApplicationTools.cs
+++ CryptoSbmScanner/ApplicationTools.cs
@@ -64,15 +64,4 @@ static class ApplicationTools
         timer.Enabled = msec > 0;
     }
 
-
-    public static void InitTimerInterval(this System.Windows.Forms.Timer timer, double seconds)
-    {
-        int msec = (int)(seconds * 1000);
-
-        timer.Enabled = false;
-        // Pas op, een interval van 0 mag niet
-        if (seconds > 0)
-            timer.Interval = msec;
-        timer.Enabled = msec > 0;
-    }
 }
