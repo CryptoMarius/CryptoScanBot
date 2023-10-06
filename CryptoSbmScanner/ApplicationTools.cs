1ef5800fea7a20d86216d577b9bb3d6dae387dcf
Marius <59998287+M22arius494@users.noreply.github.com>
2023-10-06 11:32:06 +0200

    Refactoring windows location

--- CryptoSbmScanner/ApplicationTools.cs
+++ CryptoSbmScanner/ApplicationTools.cs
@@ -5,7 +5,7 @@ namespace CryptoSbmScanner;
 static class ApplicationTools
 {
 
-    public static void SaveWindowLocation(this Form form, bool saveInstant = true)
+    public static void SaveWindowLocation(Form form)
     {
         GlobalData.Settings.General.WindowPosition = form.DesktopBounds;
 
@@ -15,11 +15,43 @@ static class ApplicationTools
             FormWindowState.Normal or FormWindowState.Maximized => form.WindowState,
             _ => FormWindowState.Normal,
         };
-
-        if (saveInstant)
-            GlobalData.SaveSettings();
     }
 
+    public static void WindowLocationRestore(Form form)
+    {
+        // this is the default
+        form.WindowState = FormWindowState.Normal;
+        form.StartPosition = FormStartPosition.WindowsDefaultBounds;
+
+        // check if the saved bounds are nonzero and visible on any screen
+        if (GlobalData.Settings.General.WindowPosition != Rectangle.Empty && IsVisibleOnAnyScreen(GlobalData.Settings.General.WindowPosition))
+        {
+            // first set the bounds
+            form.StartPosition = FormStartPosition.Manual;
+            form.DesktopBounds = GlobalData.Settings.General.WindowPosition;
+
+            // afterwards set the window state to the saved value (which could be Maximized)
+            form.WindowState = GlobalData.Settings.General.WindowState;
+        }
+        else
+        {
+            // this resets the upper left corner of the window to windows standards
+            form.StartPosition = FormStartPosition.WindowsDefaultLocation;
+
+            // we can still apply the saved size
+            // msorens: added gatekeeper, otherwise first time appears as just a title bar!
+            if (GlobalData.Settings.General.WindowPosition != Rectangle.Empty)
+            {
+                form.Size = GlobalData.Settings.General.WindowPosition.Size;
+            }
+        }
+    }
+
+
+    private static bool IsVisibleOnAnyScreen(Rectangle rect)
+      => Screen.AllScreens.Any(screen => screen.WorkingArea.IntersectsWith(rect));
+
+    
 
     public static void InitTimerInterval(this System.Timers.Timer timer, double seconds)
     {
