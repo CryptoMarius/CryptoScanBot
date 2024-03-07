f34ba2e1bec145d554db25728aa515b192d73e20
Marius <59998287+M22arius494@users.noreply.github.com>
2024-03-07 17:39:44 +0100

    Kolommen aan- of uitzetten via dit formulier

---
+++ CryptoScanBot/CryptoDataGridColumns.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CryptoScanBot;
+
+partial class CryptoDataGridColumns
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        flowLayoutPanel = new FlowLayoutPanel();
+        panelButtons = new Panel();
+        buttonCancel = new Button();
+        buttonOk = new Button();
+        panelButtons.SuspendLayout();
+        SuspendLayout();
+        // 
+        // flowLayoutPanel
+        // 
+        flowLayoutPanel.AutoSize = true;
+        flowLayoutPanel.Dock = DockStyle.Fill;
+        flowLayoutPanel.FlowDirection = FlowDirection.TopDown;
+        flowLayoutPanel.Location = new Point(0, 0);
+        flowLayoutPanel.Name = "flowLayoutPanel";
+        flowLayoutPanel.Padding = new Padding(5);
+        flowLayoutPanel.Size = new Size(387, 434);
+        flowLayoutPanel.TabIndex = 1;
+        // 
+        // panelButtons
+        // 
+        panelButtons.Controls.Add(buttonCancel);
+        panelButtons.Controls.Add(buttonOk);
+        panelButtons.Dock = DockStyle.Bottom;
+        panelButtons.Location = new Point(0, 434);
+        panelButtons.Margin = new Padding(4, 3, 4, 3);
+        panelButtons.Name = "panelButtons";
+        panelButtons.Size = new Size(387, 46);
+        panelButtons.TabIndex = 2;
+        // 
+        // buttonCancel
+        // 
+        buttonCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        buttonCancel.Location = new Point(286, 10);
+        buttonCancel.Margin = new Padding(4, 3, 4, 3);
+        buttonCancel.Name = "buttonCancel";
+        buttonCancel.Size = new Size(88, 27);
+        buttonCancel.TabIndex = 1;
+        buttonCancel.Text = "&Cancel";
+        buttonCancel.UseVisualStyleBackColor = true;
+        buttonCancel.Click += buttonCancel_Click;
+        // 
+        // buttonOk
+        // 
+        buttonOk.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        buttonOk.Location = new Point(190, 10);
+        buttonOk.Margin = new Padding(4, 3, 4, 3);
+        buttonOk.Name = "buttonOk";
+        buttonOk.Size = new Size(88, 27);
+        buttonOk.TabIndex = 0;
+        buttonOk.Text = "&Ok";
+        buttonOk.UseVisualStyleBackColor = true;
+        buttonOk.Click += buttonOk_Click;
+        // 
+        // CryptoDataGridColumns
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(387, 480);
+        Controls.Add(flowLayoutPanel);
+        Controls.Add(panelButtons);
+        Name = "CryptoDataGridColumns";
+        Text = "DataGridColumnsSelect";
+        panelButtons.ResumeLayout(false);
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private FlowLayoutPanel flowLayoutPanel;
+    private Panel panelButtons;
+    private Button buttonCancel;
+    private Button buttonOk;
+}
---
+++ CryptoScanBot/CryptoDataGridColumns.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CryptoScanBot.Settings;
+
+namespace CryptoScanBot;
+
+public partial class CryptoDataGridColumns : Form
+{
+    private List<CheckBox> List = [];
+    private CryptoDataGrid Grid;
+
+    public CryptoDataGridColumns()
+    {
+        InitializeComponent();
+    }
+
+
+    public void AddColumns(CryptoDataGrid grid)
+    {
+        Grid = grid;
+
+        foreach (DataGridViewColumn column in Grid.Grid.Columns)
+        {
+            if (!Grid.ColumnList.TryGetValue(column.HeaderText, out ColumnSetting columnSetting))
+            {
+                columnSetting = new();
+                Grid.ColumnList.Add(column.HeaderText, columnSetting);
+            }
+
+            CheckBox item = new()
+            {
+                Tag = column,
+                Text = column.HeaderText,
+                Size = new Size(100, 22),
+                CheckState = CheckState.Unchecked,
+                Checked = columnSetting.Visible,
+            };
+            flowLayoutPanel.Controls.Add(item);
+            List.Add(item);
+        }
+    }
+
+    public void FormOkay()
+    {
+        int count = 0;
+        foreach (var item in List)
+        {
+            if (!Grid.ColumnList.TryGetValue(item.Text, out ColumnSetting columnSetting))
+            {
+                columnSetting = new();
+                Grid.ColumnList.Add(item.Text, columnSetting);
+            }
+
+            columnSetting.Visible = item.Checked;
+            if (columnSetting.Visible)
+                count++;
+        }
+
+        // Restore a column (otherwise the complete header is gone)
+        if (count == 0)
+        {
+            Grid.ColumnList.Values[0].Visible = true;
+        }
+
+    }
+
+    private void buttonOk_Click(object sender, EventArgs e)
+    {
+        FormOkay();
+        DialogResult = DialogResult.OK;
+    }
+
+    private void buttonCancel_Click(object sender, EventArgs e)
+    {
+        DialogResult = DialogResult.Cancel;
+    }
+}
