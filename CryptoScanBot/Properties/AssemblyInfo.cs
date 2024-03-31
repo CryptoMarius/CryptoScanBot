ad3bee946deea3d7249b019ee8a2941dc52f4dad
Marius <59998287+M22arius494@users.noreply.github.com>
2024-03-31 14:40:44 +0200

    Versienummer opgehoogd

--- CryptoScanBot/Properties/AssemblyInfo.cs
+++ CryptoScanBot/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.1.0325")]
-[assembly: AssemblyFileVersion("1.9.1.0325")]
+[assembly: AssemblyVersion("1.9.1.0330")]
+[assembly: AssemblyFileVersion("1.9.1.0330")]
 
 [assembly: SupportedOSPlatform("windows")]
