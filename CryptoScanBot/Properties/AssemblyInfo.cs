6b64c8a12501bc0099f337d7e7d72e68de0e6572
Marius <59998287+M22arius494@users.noreply.github.com>
2024-04-12 14:33:22 +0200

    Packages changes + version number

--- CryptoScanBot/Properties/AssemblyInfo.cs
+++ CryptoScanBot/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.1.0")]
-[assembly: AssemblyFileVersion("1.9.1.0")]
+[assembly: AssemblyVersion("1.9.2.0")]
+[assembly: AssemblyFileVersion("1.9.2.0")]
 
 [assembly: SupportedOSPlatform("windows")]
