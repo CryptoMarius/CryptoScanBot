f494ef022ce604bc3217701e734fedecccf85a6a
Marius <59998287+M22arius494@users.noreply.github.com>
2024-04-30 08:06:43 +0200

    Added fixed values to enumeration

--- CryptoScanBot.Core/Enums/CryptoSignalStrategy.cs
+++ CryptoScanBot.Core/Enums/CryptoSignalStrategy.cs
@@ -3,45 +3,52 @@
 // Basis strategie en dan een long en een short variant hierop
 public enum CryptoSignalStrategy // CryptoStrategy
 {
-    Jump, // Alleen informatief?
+    Jump = 0, // Alleen informatief?
 
-    Sbm1,
-    Sbm2,
-    Sbm3,
-    Sbm4, // is er niet meer, blijft bestaan vanwege de enumeratie
-    Sbm5, // is er niet meer, blijft bestaan vanwege de enumeratie
-    Stobb,
+    Sbm1 = 1,
+    Sbm2 = 2,
+    Sbm3 = 3,
+    Sbm4 = 4, // is er niet meer, blijft bestaan vanwege de enumeratie
+    Sbm5 = 5, // is er niet meer, blijft bestaan vanwege de enumeratie
+    Stobb = 6,
 
     // Experimental
-    Flux, // is er niet meer, blijft bestaan vanwege de enumeratie
-    BullishEngulfing, // zwak
-    IchimokuKumoBreakout, // nazoeken, in de juiste positieve of negatieve trend doet ie het prima
+    Flux = 7, // is er niet meer, blijft bestaan vanwege de enumeratie
+    BullishEngulfing = 8, // zwak
+    IchimokuKumoBreakout = 9, // nazoeken, in de juiste positieve of negatieve trend doet ie het prima
 
     StoRsi, // WGHM - STOSCH en RSI momentum indicator
 #if EXTRASTRATEGIESSLOPEEMA
-    SlopeEma50,
-    SlopeEma20,
+    SlopeEma50 = 10,
+    SlopeEma20 = 11,
 #endif
 #if EXTRASTRATEGIESSLOPESMA
-    SlopeSma20,
-    SlopeSma50,
+    SlopeSma20 = 12,
+    SlopeSma50 = 13,
 #endif
 #if EXTRASTRATEGIESSLOPEKELTNER
-    SlopeKeltner,
+    SlopeKeltner = 14,
 #endif
 #if EXTRASTRATEGIESPSARRSI
-    PSarRsi,
+    PSarRsi = 15,
 #endif
+#if EXTRASTRATEGIESDPO
+    Dpo = 16,
+#endif
+#if EXTRASTRATEGIESFISHER
+    Fisher = 17,
+#endif
+
 #if EXTRASTRATEGIES
-    MacdLt, // Vervallen, opgepikt uit de groep van Marco (een ziens of dit inderdaad werkt)
-    MacdTest, // Vervallen, Nieuw idee maar dat bevalt  niet
+    MacdLt = 18, // Vervallen, opgepikt uit de groep van Marco (een ziens of dit inderdaad werkt)
+    MacdTest = 19, // Vervallen, Nieuw idee maar dat bevalt  niet
 
 
-    //// Vervallen?
-    PriceCrossedSma20,
-    PriceCrossedSma50,
-    PriceCrossedEma20,
-    PriceCrossedEma50,
-    EmaCross926,
+    // Alleen relevant in een sterke up- of downtrend (vervallen?
+    PriceCrossedSma20 = 20,
+    PriceCrossedSma50 = 21,
+    PriceCrossedEma20 = 22,
+    PriceCrossedEma50 = 23,
+    EmaCross926 = 24,
 #endif
 }
