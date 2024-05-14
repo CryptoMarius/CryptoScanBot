e3f39f7b9b1135f75f1eca53f00e3f6a89aabd98
Marius <59998287+M22arius494@users.noreply.github.com>
2024-05-14 17:56:51 +0200

    Initializing AppName

--- ExchangeTest/Program.cs
+++ ExchangeTest/Program.cs
@@ -1,3 +1,7 @@
+using System.Reflection;
+
+using CryptoScanBot.Core.Intern;
+
 namespace ExchangeTest;
 
 internal static class Program
@@ -8,9 +12,24 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        InitializeApplicationVariables();
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
+    }
+
+
+    public static void InitializeApplicationVariables()
+    {
+        GlobalData.AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        GlobalData.AppPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+        var assembly = Assembly.GetExecutingAssembly().GetName();
+        string appVersion = assembly.Version.ToString();
+        while (appVersion.EndsWith(".0"))
+            appVersion = appVersion[0..^2];
+
+        GlobalData.AppVersion = appVersion;
     }
 }
