f6504a37892d9d70e69df170f09f4ad55a5085c9
Marius <59998287+M22arius494@users.noreply.github.com>
2024-06-02 14:32:06 +0200

    Added Emulater, deletion of previous backtest data

--- ExchangeTest/Emulator/Emulator.cs
+++ ExchangeTest/Emulator/Emulator.cs
@@ -1,6 +1,11 @@
-﻿using CryptoScanBot.Core.Enums;
+﻿using System.Text;
+
+using CryptoScanBot.Core.Context;
+using CryptoScanBot.Core.Enums;
 using CryptoScanBot.Core.Intern;
 using CryptoScanBot.Core.Model;
+
+using Dapper;
 
 namespace ExchangeTest.Emulator;
 
@@ -8,47 +13,92 @@ public class Emulator
 {
     private static void DeletePreviousData()
     {
-        /*
-        // clean previous signals from the emulator (db) "delete from signal where BackTest = 1"
-        delete from signal (ja, voorlopig allemaal, wellicht nog een veld toevoegen?)
+        StringBuilder b = new();
+        using var database = new CryptoDatabase();
 
-        // clean previous assets from the emulator (db)
-        delete from asset where asset.Id in
-        (select asset.id from asset
-        inner join TradeAccount on asset.TradeAccountId = TradeAccount.Id
-        where TradeAccount.TradeAccountType = 0
-        )
 
-        // clean previous positions from the emulator (db) "delete from positionStep where BackTest = 1"?
-        delete from positionStep where positionstep.PositionId in
-        (select position.id from position
-        inner join TradeAccount on position.TradeAccountId = TradeAccount.Id
-        where TradeAccount.TradeAccountType = 0
-        )
+        using var transaction = database.BeginTransaction();
 
-        delete from positionPart where positionPart.PositionId in
-        (select position.id from position
-        inner join TradeAccount on position.TradeAccountId = TradeAccount.Id
-        where TradeAccount.TradeAccountType = 0
-        )
+        // clean signals
+        b.Clear();
+        b.AppendLine("delete from signal where BackTest=1");
+        database.Connection.Execute(b.ToString(), transaction);
 
-        delete from position where position.Id in
-        (select position.id from position
-        inner join TradeAccount on position.TradeAccountId = TradeAccount.Id
-        where TradeAccount.TradeAccountType = 0
-        )
-        */
+
+        // clean positionsteps
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting positions 1/3");
+        b.AppendLine("delete from positionStep where positionstep.PositionId in");
+        b.AppendLine("(select position.id from position");
+        b.AppendLine("inner join TradeAccount on position.TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        database.Connection.Execute(b.ToString(), transaction);
+
+
+        // clean positionparts
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting positions 2/3");
+        b.AppendLine("delete from positionPart where positionPart.PositionId in");
+        b.AppendLine("(select position.id from position");
+        b.AppendLine("inner join TradeAccount on position.TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        b.AppendLine(")");
+        database.Connection.Execute(b.ToString(), transaction);
+
+
+        // clean positions
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting positions 3/3");
+        b.AppendLine("delete from position where position.Id in");
+        b.AppendLine("(select position.id from position");
+        b.AppendLine("inner join TradeAccount on position.TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        b.AppendLine(")");
+        database.Connection.Execute(b.ToString(), transaction);
+
+
+        // clean orders
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting orders");
+        b.AppendLine("delete from [order] where [order].Id in");
+        b.AppendLine("(select [order].id from [order]");
+        b.AppendLine("inner join TradeAccount on [order].TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        b.AppendLine(")");
+        database.Connection.Execute(b.ToString(), transaction);
+
+
+        // clean trades
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting trades");
+        b.AppendLine("delete from [trade] where [trade].Id in");
+        b.AppendLine("(select [trade].id from [trade]");
+        b.AppendLine("inner join TradeAccount on [trade].TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        b.AppendLine(")");
+        database.Connection.Execute(b.ToString(), transaction);
+
+
+        // clean assets
+        b.Clear();
+        GlobalData.AddTextToLogTab("Deleting positions 3/3");
+        b.AppendLine("delete from asset where asset.Id in");
+        b.AppendLine("(select asset.id from asset");
+        b.AppendLine("inner join TradeAccount on asset.TradeAccountId = TradeAccount.Id");
+        b.AppendLine("where TradeAccount.TradeAccountType = 0");
+        b.AppendLine(")");
+        database.Connection.Execute(b.ToString(), transaction);
+
+        transaction.Commit();
     }
 
     public static async Task Execute(CryptoSymbol btcSymbol, CryptoSymbol symbol)
     {
         GlobalData.AddTextToLogTab($"Emulator {symbol.Name} started");
 
-        // todo:
-        // Adjust GetLuxIndicator (only uses the last 30 candles, needs start and enddate)
-        // rename TradingConfig to Config (not the Trader prefix because it is confusing)
-        // todo: The trend calculation is really really slow! Takes hours more if activated!!!
-        // (gebruikt ook het volledige aantal candles, 
+        // (done) Adjust GetLuxIndicator (only uses the last 30 candles, needs start and enddate)
+        // (todo) rename TradingConfig to Config (not the Trader prefix because it is confusing)
+        // (done) The trend calculation is really really slow! Takes hours more if activated!!!
 
         DeletePreviousData();
 
