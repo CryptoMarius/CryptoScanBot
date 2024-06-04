0577e53dc965a00447d3b06758b3daeb2f9f59e7
Marius <59998287+M22arius494@users.noreply.github.com>
2024-06-04 09:43:24 +0200

    Simplifield backtest dialog

--- CryptoScanBot.Core/Settings/SettingsBackTest.cs
+++ CryptoScanBot.Core/Settings/SettingsBackTest.cs
@@ -6,9 +6,10 @@ namespace CryptoScanBot.Core.Settings;
 public class SettingsBackTest
 {
     public string BackTestSymbol { get; set; } = "BTCUSDT";
-    public string BackTestInterval { get; set; } = "1M";
-    public DateTime BackTestTime { get; set; } = DateTime.Now;
-    public CryptoOrderSide BackTestMode { get; set; } = CryptoOrderSide.Buy;
-    public CryptoSignalStrategy BackTestAlgoritm { get; set; } = CryptoSignalStrategy.Sbm1;
+    //public string BackTestInterval { get; set; } = "1M";
+    public DateTime BackTestStartTime { get; set; } = DateTime.Now;
+    public DateTime BackTestEndTime { get; set; } = DateTime.Now;
+    //public CryptoOrderSide BackTestMode { get; set; } = CryptoOrderSide.Buy;
+    //public CryptoSignalStrategy BackTestAlgoritm { get; set; } = CryptoSignalStrategy.Sbm1;
 }
 
--- CryptoScanBot/AskSymbolDialog.Designer.cs
+++ CryptoScanBot/AskSymbolDialog.Designer.cs
@@ -28,28 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            EditInterval = new ComboBox();
             EditSymbol = new TextBox();
-            EditTime = new TextBox();
             label1 = new Label();
-            label2 = new Label();
             label3 = new Label();
             ButtonCancel = new Button();
             ButtonOk = new Button();
-            label4 = new Label();
-            EditAlgoritm = new ComboBox();
+            label2 = new Label();
+            EditTimeStart = new DateTimePicker();
+            EditTimeEnd = new DateTimePicker();
             SuspendLayout();
-            // 
-            // EditInterval
-            // 
-            EditInterval.AccessibleRole = AccessibleRole.IpAddress;
-            EditInterval.DropDownStyle = ComboBoxStyle.DropDownList;
-            EditInterval.FormattingEnabled = true;
-            EditInterval.Location = new Point(98, 56);
-            EditInterval.Margin = new Padding(2);
-            EditInterval.Name = "EditInterval";
-            EditInterval.Size = new Size(151, 23);
-            EditInterval.TabIndex = 71;
             // 
             // EditSymbol
             // 
@@ -58,13 +45,6 @@
             EditSymbol.Name = "EditSymbol";
             EditSymbol.Size = new Size(151, 23);
             EditSymbol.TabIndex = 70;
-            // 
-            // EditTime
-            // 
-            EditTime.Location = new Point(98, 84);
-            EditTime.Name = "EditTime";
-            EditTime.Size = new Size(151, 23);
-            EditTime.TabIndex = 72;
             // 
             // label1
             // 
@@ -75,28 +55,19 @@
             label1.TabIndex = 73;
             label1.Text = "Symbol";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(21, 59);
-            label2.Name = "label2";
-            label2.Size = new Size(46, 15);
-            label2.TabIndex = 74;
-            label2.Text = "Interval";
-            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(21, 87);
+            label3.Location = new Point(21, 60);
             label3.Name = "label3";
-            label3.Size = new Size(33, 15);
+            label3.Size = new Size(58, 15);
             label3.TabIndex = 75;
-            label3.Text = "Time";
+            label3.Text = "Start time";
             // 
             // ButtonCancel
             // 
             ButtonCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            ButtonCancel.Location = new Point(161, 161);
+            ButtonCancel.Location = new Point(161, 126);
             ButtonCancel.Margin = new Padding(4, 3, 4, 3);
             ButtonCancel.Name = "ButtonCancel";
             ButtonCancel.Size = new Size(88, 27);
@@ -108,7 +79,7 @@
             // ButtonOk
             // 
             ButtonOk.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            ButtonOk.Location = new Point(65, 161);
+            ButtonOk.Location = new Point(65, 126);
             ButtonOk.Margin = new Padding(4, 3, 4, 3);
             ButtonOk.Name = "ButtonOk";
             ButtonOk.Size = new Size(88, 27);
@@ -117,41 +88,46 @@
             ButtonOk.UseVisualStyleBackColor = true;
             ButtonOk.Click += ButtonOk_Click;
             // 
-            // label4
+            // label2
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(21, 115);
-            label4.Name = "label4";
-            label4.Size = new Size(60, 15);
-            label4.TabIndex = 77;
-            label4.Text = "Algoritme";
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 15);
+            label2.TabIndex = 77;
+            label2.Text = "End";
             // 
-            // EditAlgoritm
+            // EditTimeStart
             // 
-            EditAlgoritm.AccessibleRole = AccessibleRole.IpAddress;
-            EditAlgoritm.DropDownStyle = ComboBoxStyle.DropDownList;
-            EditAlgoritm.FormattingEnabled = true;
-            EditAlgoritm.Location = new Point(98, 112);
-            EditAlgoritm.Margin = new Padding(2);
-            EditAlgoritm.Name = "EditAlgoritm";
-            EditAlgoritm.Size = new Size(151, 23);
-            EditAlgoritm.TabIndex = 76;
+            EditTimeStart.CustomFormat = "yyyy.MM.dd HH:mm";
+            EditTimeStart.Format = DateTimePickerFormat.Custom;
+            EditTimeStart.Location = new Point(98, 58);
+            EditTimeStart.Name = "EditTimeStart";
+            EditTimeStart.Size = new Size(200, 23);
+            EditTimeStart.TabIndex = 78;
+            // 
+            // EditTimeEnd
+            // 
+            EditTimeEnd.CustomFormat = "yyyy.MM.dd HH:mm";
+            EditTimeEnd.Format = DateTimePickerFormat.Custom;
+            EditTimeEnd.Location = new Point(98, 88);
+            EditTimeEnd.Name = "EditTimeEnd";
+            EditTimeEnd.Size = new Size(200, 23);
+            EditTimeEnd.TabIndex = 79;
             // 
             // AskSymbolDialog
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(264, 200);
-            Controls.Add(label4);
-            Controls.Add(EditAlgoritm);
+            ClientSize = new Size(264, 172);
+            Controls.Add(EditTimeEnd);
+            Controls.Add(EditTimeStart);
+            Controls.Add(label2);
             Controls.Add(ButtonCancel);
             Controls.Add(ButtonOk);
             Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(EditTime);
             Controls.Add(EditSymbol);
-            Controls.Add(EditInterval);
             Name = "AskSymbolDialog";
             Text = "Backtest";
             ResumeLayout(false);
@@ -159,15 +135,13 @@
         }
 
         #endregion
-        private ComboBox EditInterval;
         private TextBox EditSymbol;
-        private TextBox EditTime;
         private Label label1;
-        private Label label2;
         private Label label3;
         private Button ButtonCancel;
         private Button ButtonOk;
-        private Label label4;
-        private ComboBox EditAlgoritm;
+        private Label label2;
+        private DateTimePicker EditTimeStart;
+        private DateTimePicker EditTimeEnd;
     }
 }
