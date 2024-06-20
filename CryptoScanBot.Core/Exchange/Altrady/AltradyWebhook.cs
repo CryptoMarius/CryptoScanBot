0f89648fbf6e53c884ae53bd95b47a8655a9da2d
Marius <59998287+M22arius494@users.noreply.github.com>
2024-06-20 19:47:57 +0200

    Activated Altrady webhook

--- CryptoScanBot.Core/Exchange/Altrady/AltradyWebhook.cs
+++ CryptoScanBot.Core/Exchange/Altrady/AltradyWebhook.cs
@@ -1,180 +1,127 @@
-﻿//using CryptoScanBot.Core.Intern;
-//using CryptoScanBot.Core.Model;
-//using Newtonsoft.Json.Linq;
-//using System.Net;
+﻿using CryptoScanBot.Core.Intern;
+using CryptoScanBot.Core.Model;
 
-// Opmerking:
-// De Altrady webhook is qua paperTesting totaal afgekeurd, het vult DCA orders op -10% en 
-// sluit dan de order met winst met als gevolg een totaal vertekend beeld onstaat van 
-// het traden. (Vraag: hoe vaak is het eerder fout gegaan?, niet iets om te negeren)
-// Zo kun je maandenlang positief papertraden terwijl de werkelijkheid totaal anders is
+using Newtonsoft.Json.Linq;
+
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace CryptoScanBot.Core.Exchange.Altrady;
 
+public class AltradyWebhook
+{
 
-//public class AltradyWebhook
-//{
-
-//    private static void Execute(CryptoSignal signal, string botKey, string botSecret, decimal stoppLoss)
-//    {
-//        HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.altrady.com/v2/signal_bot_positions");
-//        httpWebRequest.ContentType = "application/json";
-//        httpWebRequest.Method = "POST";
-
-//        try
-//        {
-//            //"test": true,
-//            //"api_key": "string",
-//            //"api_secret": "string",
-//            //"side": "long",
-//            //"exchange": "string",
-//            //"symbol": "string",
-//            //"signal_price": 0,
-
-//            dynamic request = new JObject();
-//            //request.test = false;
-//            request.api_key = botKey;
-//            request.api_secret = botSecret;
-//            request.side = "long";
-//            request.exchange = "binance";
-//            request.symbol = signal.Symbol.Name;
-//            // Afgesterd - 27-04-2023 10:12 (na verwarrende buy's die suggeren dat er veel te hoog gekocht werd! <papertrading sucks>)
-//            // vervelende is dat de LastPrice kan varieeren, je zou nog steeds best hoog kunnen inkopen (niet de schuld van deze code)
-//            //request.signal_price = signal.Symbol.LastPrice + (GlobalData.Settings.Bot.GlobalBuyVarying * signal.Symbol.LastPrice) / 100m;
-
-//            // 27-04-2023 10:12 signal_price Aangezet na verwarrende entries
-//            // 28-04-2023 10:12 signal_price afgesterd na aanpassing SBM2 berekening
-
-//            CryptoSymbolInterval symbolPeriod = signal.Symbol.GetSymbolInterval(signal.Interval.IntervalPeriod);
-//            CryptoCandle candleLast = symbolPeriod.CandleList.Values.Last();
-//            //// We zetten de buy order op de candle.close
-//            //decimal? value = candleLast.Close;
-//            //// We zetten de buy order op de gemiddelde van de laatste candle
-//            //decimal? value = (candleLast.Open + candleLast.Close) / 2m;
-//            //// We zetten de buy order op de bb, want daar zakt ie toch naar terug (nouja, vaak)
-//            //decimal? value = (decimal)candleLast.CandleData.BollingerBandsLowerBand + 2 * signal.Symbol.PriceTickSize;
-//            //if (signal.Symbol.LastPrice < (decimal)value)
-//            //    value = signal.Symbol.LastPrice;
-//            //request.signal_price = value - signal.Symbol.PriceTickSize;
-//            // En zonder de last_price is het een marketorder?
-
-//            // wellicht is eea afhankelijk van de markt stemming,
-//            // bij een positieve stemming is een ~marketorder okay
-//            // bij een negatieve stemming is de bb.lower een betere keuze
-
-//            //"take_profit": [
-//            //{
-//            //"price_percentage": 0,
-//            //"position_percentage": 0
-//            //}
-//            //],
-//            dynamic take_ProfitList = new JArray(); // List<dynamic>();
-//            request.take_profit = take_ProfitList;
-
-//            dynamic take_Profit1 = new JObject();
-//            take_Profit1.price_percentage = GlobalData.Settings.Trading.ProfitPercentage;
-//            take_Profit1.position_percentage = 100;
-//            take_ProfitList.Add(take_Profit1);
-
-//            //dynamic take_Profit2 = new JObject();
-//            //take_Profit2.price_percentage = 0.50;
-//            //take_Profit2.position_percentage = 50;
-//            //take_ProfitList.Add(take_Profit2);
-
-//            //"stop_loss": 
-//            //{
-//            //"stop_percentage": 0,
-//            //"cool_down_amount": 0,
-//            //"cool_down_time_frame": "minute"
-//            //}
-
-//            if (stoppLoss > 0)
-//            {
-//                dynamic stop_loss = new JObject();
-//                request.stop_loss = stop_loss;
-//                stop_loss.stop_percentage = stoppLoss;
-//                stop_loss.cool_down_amount = 0;
-//                stop_loss.cool_down_time_frame = "minute";
-//            }
-
-//            string json = request.ToString();
-//            //Console.WriteLine(json);
-
-//            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-//            {
-//                streamWriter.Write(json);
-//                //GlobalData.AddTextToLogTab(json);
-//            }
-//            //GlobalData.AddTextToLogTab(signal.Symbol.Name + " " + signal.Interval.Name + " verzonden naar Altrady webhook");
-//            //GlobalData.AddTextToTelegram(signal.Symbol.Name + " " + signal.Interval.Name + " verzonden naar Altrady webhook");
-//            //GlobalData.AddTextToLogTab(json);
+    public static async void DelegateControlToAltrady(CryptoPosition position)
+    {
+        if (GlobalData.AltradyApi.Key == "" || GlobalData.AltradyApi.Secret == "")
+        {
+            GlobalData.AddTextToLogTab($"{position.Symbol.Name} {position.Interval!.Name} unable to send to Altrady webhook, no api key's available");
+            return;
+        }
 
 
-//            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-//            using StreamReader streamReader = new(httpResponse.GetResponseStream());
-//            var result = streamReader.ReadToEnd();
-//            //GlobalData.AddTextToLogTab(result);
+        GlobalData.AddTextToLogTab($"{position.Symbol.Name} {position.Interval!.Name} send to Altrady webhook LastTradeDate={position.Symbol.LastTradeDate}");
+
+        try
+        {
+            // some documentation (nicely done, thanks!)
+            // https://support.altrady.com/en/article/webhook-signals-testing-and-errors-1pl7g40/
+            // https://support.altrady.com/en/article/webhook-signals-open-close-increase-or-reverse-a-position-5sr46f/#4-optional-settings-for-the-open-and-reverse-signal
+            dynamic request = new JObject();
+
+            // Request body
+            request.test = false;
+            request.action = "open";
+            if (position.Side == Enums.CryptoTradeSide.Long)
+                request.side = "long";
+            else
+                request.side = "short";
+            request.api_key = GlobalData.AltradyApi.Key;
+            request.api_secret = GlobalData.AltradyApi.Secret;
+
+            // TODO, codes look at the SettingsLink..
+            request.exchange = "BYBI";  //"BINA";
+            request.symbol = $"BYBI_{position.Symbol.Quote}_{position.Symbol.Base}";
+            request.signal_price = position.EntryPrice;
+            request.adjust_fee = true;
+
+            // TP body (just 1)
+            {
+                dynamic tp_orders = new JArray();
+                request.take_profit = tp_orders;
+
+                dynamic tp = new JObject();
+                tp_orders.Add(tp);
+
+                tp.price_percentage = GlobalData.Settings.Trading.ProfitPercentage;
+                tp.position_percentage = 100;
+            }
+
+            // DCA body (multiple)
+            // Is going to be expensive with my 5 dca setup... ;-)
+            if (GlobalData.Settings.Trading.DcaList.Count > 0)
+            {
+                dynamic dca_orders = new JArray();
+                request.dca_orders = dca_orders;
+
+                foreach (var dcaItem in GlobalData.Settings.Trading.DcaList)
+                {
+                    dynamic dca = new JObject();
+                    dca_orders.Add(dca);
+
+                    dca.price_percentage = dcaItem.Percentage;
+                    dca.quantity_percentage = dcaItem.Factor;
+                }
+            }
+
+            // SL body // Or was it the limitPercentage?
+            if (GlobalData.Settings.Trading.StopLossPercentage > 0)
+            {
+                dynamic stop_loss = new JObject();
+                request.stop_loss = stop_loss;
+                stop_loss.stop_percentage = GlobalData.Settings.Trading.StopLossPercentage;
+                stop_loss.cool_down_amount = 0;
+                stop_loss.cool_down_time_frame = "minute";
+            }
 
 
+            // send
 
-//            //?? nieuwe web
-//            //using HttpClient httpClient = new();
-//            ////HttpRequestMessage request2 = new HttpRequestMessage(HttpMethod.Post, "https://api.altrady.com/v2/signal_bot_positions");
-//            //var content = new StringContent(json, Encoding.UTF8, "application/json");
-//            //var result = httpClient.PostAsync("https://api.altrady.com/v2/signal_bot_positions", content).Result;
+            // todo, replace obsolete WebRequest with HttpClient..
+            //HttpClient httpClient = new();
+            //var jsonData = await httpClient.GetFromJsonAsync<request>("https://api.alternative.me/fng/");
+            //await httpClient.SendAsync("/api/v3/exchangeInfo", HttpMethod.Post);????
 
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.altrady.com/v2/signal_bot_positions");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
 
+            string json = request.ToString();
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                GlobalData.AddTextToLogTab(json);
+            }
+            GlobalData.AddTextToLogTab($"{position.Symbol.Name} {position.Interval!.Name} send to Altrady webhook");
+            ScannerLog.Logger.Trace($"{position.Symbol.Name} {position.Interval.Name} send to Altrady webhook {json}");
 
-//            //string href = string.Format("https://api.altrady.com/v2/signal_bot_positions/");
-//            //webClient.UploadData(new Uri(href), downLoadFolder);
-//        }
-//        catch (Exception error)
-//        {
-//            // don care (paper account)
-//            ScannerLog.Logger.Error(error, "");
-//            GlobalData.AddTextToLogTab("error webhook " + error.ToString()); // symbol.Text + " " + 
-//        }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using StreamReader streamReader = new(httpResponse.GetResponseStream());
+            var result = streamReader.ReadToEnd();
 
-//    }
-//    public static void ExecuteBuy(CryptoSignal signal)
-//    {
-//        if (signal.Symbol.LastTradeDate.HasValue && signal.Symbol.LastTradeDate?.AddMinutes(GlobalData.Settings.Trading.GlobalBuyCooldownTime) > DateTime.UtcNow)
-//        {
-//            GlobalData.AddTextToLogTab(signal.Symbol.Name + " " + signal.Interval.Name + " heeft een openstaande trade of is in cooldown na een trade");
-//            return;
-//        }
+            // log response
+            //string text = JsonSerializer.Serialize(result, GlobalData.JsonSerializerIndented);
+            GlobalData.AddTextToLogTab($"{position.Symbol.Name} {position.Interval.Name} result Altrady webhook {result}");
+            ScannerLog.Logger.Trace($"{position.Symbol.Name} {position.Interval.Name} result Altrady webhook {result}");
+        }
+        catch (Exception error)
+        {
+            ScannerLog.Logger.Error(error, "");
+            GlobalData.AddTextToLogTab($"error webhook {position.Symbol.Name} {position.Interval!.Name} error={error}");
+        }
 
-//        // in de veronderstelling dat onderstaande allemaal lukt
-//        signal.Symbol.LastTradeDate = DateTime.UtcNow;
-
-
-//        // Alleen SBM doet mee in de testronde
-//        switch (signal.Strategy)
-//        {
-//            case SignalStrategy.Sbm1:
-//            case SignalStrategy.Sbm2:
-//            case SignalStrategy.Sbm3:
-//            case SignalStrategy.Sbm4:
-//            case SignalStrategy.Sbm5:
-//                if (signal.Symbol.Quote.Equals("USDT"))
-//                {
-//				    // ja, het is nog even hardcoded
-//                    Execute(signal, "95315384-4bee-4e5d-8134-82d1ba372787", "a390f9b0-f32b-46b9-84b4-f0615240aca2", 02.50m);
-//                    Execute(signal, "fbafb2eb-97ba-437e-9ea0-44c4e6eec7bb", "5d4c1ef7-4210-4273-804f-540522c48b27", 05.00m);
-//                    Execute(signal, "a36a98f3-7968-4a5c-999d-79521fe68706", "dfdd1ecd-46f2-4ad4-9268-9c988b1afa95", 10.0m);
-//                }
-//                else 
-//                if (signal.Symbol.Quote.Equals("BTC"))
-//                {
-//                    // ja, het is nog even hardcoded
-//                    Execute(signal, "52128499-cee4-41c7-99fe-ab533b236629", "d8712b2e-d7db-4724-bcef-7f8c4bd99cf0", 10.00m);
-//                }
-//                break;
+    }
 
 
-
-//        }
-//    }
-
-
-//}
+}
