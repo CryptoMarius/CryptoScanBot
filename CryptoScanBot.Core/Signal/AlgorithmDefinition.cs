f23bac344e00c83807fc75ddf1ba9f93eecaa430
Marius <59998287+M22arius494@users.noreply.github.com>
2024-06-29 10:17:02 +0200

    Made types nullable

--- CryptoScanBot.Core/Signal/AlgorithmDefinition.cs
+++ CryptoScanBot.Core/Signal/AlgorithmDefinition.cs
@@ -1,8 +1,7 @@
 ﻿using CryptoScanBot.Core.Enums;
 using CryptoScanBot.Core.Model;
-using CryptoScanBot.Core.Signal;
 
-namespace CryptoScanBot.Signal;
+namespace CryptoScanBot.Core.Signal;
 
 // Een oude enumeratie "SignalStrategy" is vervallen en overgenomen door de combinatie
 //  van Mode en SignalStrategy (zonder de oversold/overbought toevoegingen)
@@ -10,8 +9,8 @@ public class AlgorithmDefinition
 {
     public required string Name { get; set; }
     public required CryptoSignalStrategy Strategy { get; set; }
-    public required Type AnalyzeLongType { get; set; }
-    public required Type AnalyzeShortType { get; set; }
+    public required Type? AnalyzeLongType { get; set; }
+    public required Type? AnalyzeShortType { get; set; }
 
     public SignalCreateBase? InstantiateAnalyzeLong(CryptoSymbol symbol, CryptoInterval interval, CryptoCandle candle) 
         => (SignalCreateBase?)Activator.CreateInstance(AnalyzeLongType, [symbol, interval, candle]);
