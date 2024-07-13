54a9029371320419d10b15c99650621bcdae1a07
Marius <59998287+M22arius494@users.noreply.github.com>
2024-07-13 16:41:04 +0200

    Translations

--- CryptoScanBot.Core/Exchange/Mexc/Spot/Candle.cs
+++ CryptoScanBot.Core/Exchange/Mexc/Spot/Candle.cs
@@ -105,8 +105,8 @@ public class Candle
 
             CryptoSymbolInterval symbolPeriod = symbol.GetSymbolInterval(interval.IntervalPeriod);
             SortedList<long, CryptoCandle> candles = symbolPeriod.CandleList;
-            string s = symbol.Exchange.Name + " " + symbol.Name + " " + interval.Name + " fetch from " + CandleTools.GetUnixDate(startFetchDate).ToLocalTime() + " UTC tot " + CandleTools.GetUnixDate(symbolInterval.LastCandleSynchronized).ToLocalTime() + " UTC";
-            GlobalData.AddTextToLogTab(s + " received: " + result.Data.Count() + " totaal: " + candles.Count.ToString());
+            string s = symbol.Exchange.Name + " " + symbol.Name + " " + interval.Name + " fetch from " + CandleTools.GetUnixDate(startFetchDate).ToLocalTime() + " UTC .. " + CandleTools.GetUnixDate(symbolInterval.LastCandleSynchronized).ToLocalTime() + " UTC";
+            GlobalData.AddTextToLogTab(s + " received: " + result.Data.Count() + " total: " + candles.Count.ToString());
             return result.Data.Count();
         }
     }
