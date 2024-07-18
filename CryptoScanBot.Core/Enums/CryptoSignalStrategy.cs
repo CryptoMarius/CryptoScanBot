cae6d8192e9e842a54d75b3b11e60e3e550b1d53
Marius <59998287+M22arius494@users.noreply.github.com>
2024-07-18 08:10:15 +0200

    Removed experimental Ross2 strategy

--- CryptoScanBot.Core/Enums/CryptoSignalStrategy.cs
+++ CryptoScanBot.Core/Enums/CryptoSignalStrategy.cs
@@ -15,6 +15,5 @@ public enum CryptoSignalStrategy
     StoRsi = 10, // WGHM - STOSCH en RSI momentum indicator
     RsiStochK = 19,
 
-    Ross2 = 98, // just a test
     Ross = 99, // just a test
 }
