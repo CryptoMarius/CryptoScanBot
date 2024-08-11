c118336d1f78894c6d949c9ee2145357f67c29c2
Marius <59998287+M22arius494@users.noreply.github.com>
2024-08-11 13:25:19 +0200

    Trim app parameters

--- CryptoScanBot.Core/ApplicationParams.cs
+++ CryptoScanBot.Core/ApplicationParams.cs
@@ -6,12 +6,14 @@ namespace CryptoScanBot.Core;
 public class ApplicationParams
 {
 
+    public string? _AppDataFolder;
     [Option('f', "folder", Required = false, HelpText = "De te gebruiken folder in de APPDATA")]
-    public string? AppDataFolder { get; set; }
+    public string? AppDataFolder { get { return _AppDataFolder; } set { _AppDataFolder = value!.Trim(); } }
 
     // Een idee? Dan hoeven we niet zo raar te switchen (soms midden in het ophalen van candles <met alle problemen van dien>)
-    [Option('e', "exchange", Required = false, HelpText = "De te gebruiken exchange (Binance Spot, Binance Futures, Bybit Spot, ByBit Futures, Mexc Spot of Kucoin Spot")]
-    public string? ExchangeName { get; set; }
+    private string? _ExchangeName;
+    [Option('e', "exchange", Required = false, HelpText = "De te gebruiken exchange (Binance Spot, Binance Futures, Bybit Spot, ByBit Futures, Kucoin Spot of Mexc Spot)")]
+    public string? ExchangeName { get { return _ExchangeName; } set { _ExchangeName = value!.Trim(); } }
 
     static public ApplicationParams? Options { get; set; }
 
