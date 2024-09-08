c93ae7ad7d8fd86762d6073a90ec15b702513212
Marius <59998287+M22arius494@users.noreply.github.com>
2024-09-08 12:29:53 +0200

    Serializable and minor changes

--- CryptoScanBot.Core/Trend/ZigZagResult.cs
+++ CryptoScanBot.Core/Trend/ZigZagResult.cs
@@ -2,6 +2,7 @@
 
 namespace CryptoScanBot.Core.Trend;
 
+[Serializable]
 public class ZigZagResult
 {
     public required char PointType { get; set; } // indicates a specific point and type e.g. H or L
@@ -9,10 +10,10 @@ public class ZigZagResult
     public required CryptoCandle Candle { get; set; }
 
     public bool Dominant { get; set; } = false;
-    public CryptoCandle? InvalidOn { get; set; } = null;
-
 
     public decimal Top { get; set; }
     public decimal Bottom { get; set; }
     public double Percentage { get; set; }
+
+    public CryptoCandle? InvalidOn { get; set; } = null;
 }
