51b77b33fc891ae886d67927fe9b5fa666a60ecf
Marius <59998287+M22arius494@users.noreply.github.com>
2024-11-12 11:32:31 +0100

    Renamed class

--- CryptoScanBot.Core/Settings/Strategy/SettingsSignalStrategyZones.cs
+++ CryptoScanBot.Core/Settings/Strategy/SettingsSignalStrategyZones.cs
@@ -2,7 +2,7 @@
 
 namespace CryptoScanBot.Core.Settings.Strategy;
 
-public class SettingsSignalStrategyDominant : SettingsSignalStrategyBase
+public class SettingsSignalStrategyZones : SettingsSignalStrategyBase
 {
     public bool UseHighLow { get; set; } = false; // advise = open/close
     public int CandleCount { get; set; } = 3000; // 150 day's back
@@ -10,8 +10,8 @@ public class SettingsSignalStrategyDominant : SettingsSignalStrategyBase
     public CryptoIntervalPeriod Interval { get; set; } = CryptoIntervalPeriod.interval1h;
 
     // Limits unzoomed box
-    public decimal MinimumUnZoomedPercentage { get; set; } = 0.2m;
-    public decimal MaximumUnZoomedPercentage { get; set; } = 10.0m;
+    public decimal MinimumUnZoomedPercentage { get; set; } = 0.0m;
+    public decimal MaximumUnZoomedPercentage { get; set; } = 0.0m;
 
     // Limits box after zooming
     public bool ZoomLowerTimeFrames { get; set; } = true;
@@ -21,7 +21,7 @@ public class SettingsSignalStrategyDominant : SettingsSignalStrategyBase
     // Signal percentage
     public decimal WarnPercentage { get; set; } = 1.0m;
 
-    public SettingsSignalStrategyDominant() : base()
+    public SettingsSignalStrategyZones() : base()
     {
         SoundFileLong = "sound-zones-long.wav";
         SoundFileShort = "sound-zones-short.wav";
