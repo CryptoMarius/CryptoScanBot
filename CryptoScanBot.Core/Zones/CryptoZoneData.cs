8c399cfcb69747ec39bd643127705f789d78e816
Marius <59998287+M22arius494@users.noreply.github.com>
2024-12-18 20:35:28 +0100

    Simplification

--- CryptoScanBot.Core/Zones/CryptoZoneData.cs
+++ CryptoScanBot.Core/Zones/CryptoZoneData.cs
@@ -8,14 +8,14 @@ public class CryptoZoneData
     public required CryptoAccount Account { get; set; }
     public required Model.CryptoExchange Exchange { get; set; }
     public required CryptoSymbol Symbol { get; set; }
-    public required CryptoSymbolInterval SymbolInterval { get; set; }
     public required CryptoInterval Interval { get; set; }
+    public required CryptoSymbolInterval SymbolInterval { get; set; }
 
     public required ZigZagIndicator9 Indicator { get; set; }
     public required ZigZagIndicator9 IndicatorFib { get; set; }
 
     public List<CryptoSignal> Signals { get; set; } = [];
-    public List<CryptoPosition> Positions { get; set; } = [];
+    //public List<CryptoPosition> Positions { get; set; } = [];
 
     // New structure?
     //public required AccountSymbolIntervalData AccountSymbolIntervalData { get; set; }
