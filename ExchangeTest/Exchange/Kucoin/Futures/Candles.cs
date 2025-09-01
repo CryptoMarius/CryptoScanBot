763dca6fdc58ee11040cd6d0eedc681b76094208
Marius <59998287+M22arius494@users.noreply.github.com>
2025-09-01 15:36:31 +0200

    Obsolete code

--- ExchangeTest/Exchange/Kucoin/Futures/Candles.cs
+++ ExchangeTest/Exchange/Kucoin/Futures/Candles.cs
@@ -22,25 +22,6 @@ public class Candles
     // Prevent multiple sessions
     private static readonly SemaphoreSlim Semaphore = new(1);
 
-    private static FuturesKlineInterval? GetExchangeInterval(CryptoInterval interval)
-    {
-        return interval.IntervalPeriod switch
-        {
-            CryptoIntervalPeriod.interval1m => FuturesKlineInterval.OneMinute,
-            //CryptoIntervalPeriod.interval3m => FuturesKlineInterval.ThreeMinutes,
-            CryptoIntervalPeriod.interval5m => FuturesKlineInterval.FiveMinutes,
-            CryptoIntervalPeriod.interval15m => FuturesKlineInterval.FifteenMinutes,
-            CryptoIntervalPeriod.interval30m => FuturesKlineInterval.ThirtyMinutes,
-            CryptoIntervalPeriod.interval1h => FuturesKlineInterval.OneHour,
-            CryptoIntervalPeriod.interval2h => FuturesKlineInterval.TwoHours,
-            CryptoIntervalPeriod.interval4h => FuturesKlineInterval.FourHours,
-            //CryptoIntervalPeriod.interval6h => FuturesKlineInterval.SixHours,
-            CryptoIntervalPeriod.interval8h => FuturesKlineInterval.EightHours,
-            CryptoIntervalPeriod.interval12h => FuturesKlineInterval.TwelveHours,
-            CryptoIntervalPeriod.interval1d => FuturesKlineInterval.OneDay,
-            _ => null,
-        };
-    }
 
     private static async Task<long> GetCandlesForInterval(KucoinRestClient client, CryptoSymbol symbol, CryptoInterval interval, CryptoSymbolInterval symbolInterval)
     {
