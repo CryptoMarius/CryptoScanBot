6ba0e8d687e47bf4719609e8c39202366e9eb73d
Marius <59998287+M22arius494@users.noreply.github.com>
2025-09-03 16:47:17 +0200

    Kraken Futures Experiment

---
+++ CryptoScanBot.Core/Exchange/Kraken/Futures/Api.cs
@@ -0,0 +1,71 @@
+﻿using CryptoExchange.Net.Authentication;
+
+using CryptoScanBot.Core.Context;
+using CryptoScanBot.Core.Core;
+using CryptoScanBot.Core.Enums;
+using CryptoScanBot.Core.Model;
+
+using Kraken.Net.Clients;
+
+namespace CryptoScanBot.Core.Exchange.Kraken.Futures;
+
+public class Api : ExchangeBase
+{
+    [System.Diagnostics.CodeAnalysis.SetsRequiredMembersAttribute]
+    public Api()
+    {
+        //Asset = new Asset();
+        Candle = new Candle(this);
+        Symbol = new Symbol();
+        //Order = new Order();
+        //Trade = new Trade();
+    }
+
+    public override IDisposable GetClient()
+    {
+        return new KrakenRestClient();
+    }
+
+    public override void ExchangeDefaults()
+    {
+        ExchangeOptions.ExchangeName = "Kraken Futures";
+        ExchangeOptions.LimitAmountOfSymbols = false;
+        ExchangeOptions.SymbolLimitPerSubscription = 5; // onbekend
+        GlobalData.AddTextToLogTab($"{ExchangeOptions.ExchangeName} defaults");
+
+        KrakenRestClient.SetDefaultOptions(options =>
+        {
+            if (GlobalData.TradingApi.Key != "")
+                options.ApiCredentials = new ApiCredentials(GlobalData.TradingApi.Key, GlobalData.TradingApi.Secret);
+        });
+
+        KrakenSocketClient.SetDefaultOptions(options =>
+        {
+            options.ReconnectInterval = TimeSpan.FromSeconds(15);
+            if (GlobalData.TradingApi.Key != "")
+                options.ApiCredentials = new ApiCredentials(GlobalData.TradingApi.Key, GlobalData.TradingApi.Secret);
+        });
+
+        //PriceTicker = new Ticker(ExchangeOptions, typeof(SubscriptionPriceTicker), CryptoTickerType.price);
+        KLineTicker = new Ticker(ExchangeOptions, typeof(SubscriptionKLineTicker), CryptoTickerType.kline);
+        //UserTicker = new Ticker(ExchangeOptions, typeof(SubscriptionUserTicker), CryptoTickerType.user);
+    }
+
+
+    public override Task<(bool result, TradeParams? tradeParams)> PlaceOrder(CryptoDatabase database,
+        CryptoPosition position, CryptoPositionPart part, DateTime currentDate,
+        CryptoOrderType orderType, CryptoOrderSide orderSide,
+        decimal quantity, decimal price, decimal? stop, decimal? limit, bool generateJsonDebug = false)
+    {
+        // not implemented
+        return Task.FromResult<(bool succes, TradeParams? tradeParams)>((false, null));
+    }
+
+
+    public override Task<(bool succes, TradeParams? tradeParams)> Cancel(CryptoPosition position, CryptoPositionPart part, CryptoPositionStep step)
+    {
+        // not implemented
+        return Task.FromResult<(bool succes, TradeParams? tradeParams)>((false, null));
+    }
+
+}
---
+++ CryptoScanBot.Core/Exchange/Kraken/Futures/SubscriptionKLineTicker.cs
@@ -0,0 +1,59 @@
+﻿using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.Objects.Sockets;
+using CryptoExchange.Net.SharedApis;
+
+using CryptoScanBot.Core.Core;
+using CryptoScanBot.Core.Model;
+
+using Kraken.Net.Clients;
+using Kraken.Net.Objects.Models.Socket;
+
+namespace CryptoScanBot.Core.Exchange.Kraken.Futures;
+
+public class SubscriptionKLineTicker(ExchangeOptions exchangeOptions) : SubscriptionTicker(exchangeOptions)
+{
+    private async Task ProcessCandleAsync(string topic, KrakenKlineUpdate kline)
+    {
+        // De interval wordt geprefixed in de topic
+        string symbolName = topic.Replace("/", "");
+        if (GlobalData.ExchangeListName.TryGetValue(ExchangeBase.ExchangeOptions.ExchangeName, out Model.CryptoExchange? exchange))
+        {
+            if (exchange.SymbolListName.TryGetValue(symbolName, out CryptoSymbol? symbol))
+            {
+                Interlocked.Increment(ref TickerCount);
+                //GlobalData.AddTextToLogTab(String.Format("{0} Candle {1} start processing", topic, kline.Timestamp.ToLocalTime()));
+                var candle = await CandleTools.Process1mCandleAsync(symbol, kline.OpenTime, kline.OpenPrice, kline.HighPrice, kline.LowPrice, kline.ClosePrice, 0, kline.Volume);
+                GlobalData.ThreadMonitorCandle!.AddToQueue(symbol, candle);
+            }
+        }
+    }
+
+    public override async Task<CallResult<UpdateSubscription>?> Subscribe()
+    {
+        SemaphoreSlim symbolListSemaphore = new(1, 1);
+        TickerGroup!.SocketClient ??= new KrakenSocketClient();
+        var client = (KrakenSocketClient)TickerGroup.SocketClient;
+        var api = client.FuturesApi;
+
+        List<string> symbolList = [];
+        foreach (var symbol in SymbolList)
+        {
+            string symbolName = api.FormatSymbol(symbol.Base, symbol.Quote, TradingMode.PerpetualLinear);
+            symbolList.Add(symbolName);
+        }
+
+        // TODO: SubscribeToKlineUpdatesAsync does not exist?
+
+        //var subscriptionResult = await api.SubscribeToKlineUpdatesAsync(symbolList, KlineInterval.OneMinute, data =>
+        //{
+        //    foreach (KrakenKlineUpdate kline in data.Data)
+        //    {
+        //        Task.Run(async () => { await ProcessCandleAsync(data.Symbol ?? "", kline); });
+        //    }
+        //}, ExchangeBase.CancellationToken).ConfigureAwait(false);
+
+        return null;
+        //return subscriptionResult;
+    }
+
+}
