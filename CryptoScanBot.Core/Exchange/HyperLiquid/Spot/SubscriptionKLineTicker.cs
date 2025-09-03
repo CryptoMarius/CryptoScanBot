da52110c5ba029023236d03577e0f040b8c2c731
Marius <59998287+M22arius494@users.noreply.github.com>
2025-09-03 16:48:01 +0200

    Hyperliquid Experiment

---
+++ CryptoScanBot.Core/Exchange/HyperLiquid/Spot/SubscriptionKLineTicker.cs
@@ -0,0 +1,87 @@
+﻿using CryptoExchange.Net.Clients;
+using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.Objects.Sockets;
+using CryptoExchange.Net.SharedApis;
+
+using CryptoScanBot.Core.Core;
+using CryptoScanBot.Core.Model;
+
+using HyperLiquid.Net.Clients;
+using HyperLiquid.Net.Enums;
+using HyperLiquid.Net.Objects.Models;
+
+namespace CryptoScanBot.Core.Exchange.HyperLiquid.Spot;
+
+/// <summary>
+/// Monitoren van 1m candles (die gepushed worden door de exchange)
+/// </summary>
+public class SubscriptionKLineTicker(ExchangeOptions exchangeOptions) : SubscriptionTicker(exchangeOptions)
+{
+    private async Task ProcessCandleAsync(string? symbolName, HyperLiquidKline kline)
+    {
+        // Aantekeningen
+        // De Base volume is the volume in terms of the first currency pair.
+        // De Quote volume is the volume in terms of the second currency pair.
+        // For example, for "MFN/USDT": 
+        // base volume would be MFN
+        // quote volume would be USDT
+
+        //ScannerLog.Logger.Trace($"kline ticker {topic}");
+
+        // De interval wordt geprefixed in de topic "kline.1.SymbolName"
+        if (string.IsNullOrEmpty(symbolName))
+            return;
+        symbolName = symbolName.Replace("/", "");
+
+        if (GlobalData.ExchangeListName.TryGetValue(ExchangeBase.ExchangeOptions.ExchangeName, out Model.CryptoExchange? exchange))
+        {
+            if (exchange.SymbolListName.TryGetValue(symbolName, out CryptoSymbol? symbol))
+            {
+                Interlocked.Increment(ref TickerCount);
+                //ScannerLog.Logger.Trace($"kline ticker {topic} process");
+                //GlobalData.AddTextToLogTab($"{topic} Candle {kline.Timestamp.ToLocalTime()} start processing");
+
+                var candle = await CandleTools.Process1mCandleAsync(symbol, kline.OpenTime, kline.OpenPrice, kline.HighPrice, kline.LowPrice, kline.ClosePrice, kline.Volume, 0);
+                GlobalData.ThreadMonitorCandle!.AddToQueue(symbol, candle);
+
+                //if (GlobalData.Settings.General.DebugKLineReceive && (GlobalData.Settings.General.DebugSymbol == symbol.Name || GlobalData.Settings.General.DebugSymbol == ""))
+                //    GlobalData.AddTextToLogTab($"Debug candle {symbol.Name} 1m {JsonSerializer.Serialize(kline, JsonTools.JsonSerializerNotIndented)}");
+            }
+        }
+
+    }
+
+
+    public override async Task<CallResult<UpdateSubscription>?> Subscribe()
+    {
+        SemaphoreSlim symbolListSemaphore = new(1, 1);
+        TickerGroup!.SocketClient ??= new HyperLiquidSocketClient();
+        var client = (HyperLiquidSocketClient)TickerGroup.SocketClient;
+        var api = client.SpotApi;
+
+        // TODO: quick en dirty
+        List<string> symbols = [];
+        string symbolNames = "";
+        foreach (var symbol in SymbolList)
+        {
+            string symbolName = api.FormatSymbol(symbol.Base, symbol.Quote, TradingMode.Spot);
+            if (symbolNames == "")
+                symbolNames = symbolName;
+            else
+                symbolNames += "," + symbolName;
+            symbols.Add(symbolNames);
+        }
+
+        TickerGroup!.SocketClient ??= new HyperLiquidSocketClient();
+        var subscriptionResult = await api.SubscribeToKlineUpdatesAsync(symbolNames, KlineInterval.OneMinute, data =>
+        {
+            var kline = data.Data;
+            {
+                Task.Run(async () => { await ProcessCandleAsync(data.Symbol, kline); });
+            }
+        }, ExchangeBase.CancellationToken).ConfigureAwait(false);
+
+        return subscriptionResult;
+    }
+
+}
---
+++ CryptoScanBot.Core/Exchange/HyperLiquid/Spot/Symbol.cs
@@ -0,0 +1,200 @@
+﻿using CryptoExchange.Net.SharedApis;
+
+using CryptoScanBot.Core.Context;
+using CryptoScanBot.Core.Core;
+using CryptoScanBot.Core.Model;
+
+using Dapper.Contrib.Extensions;
+
+using HyperLiquid.Net.Clients;
+
+namespace CryptoScanBot.Core.Exchange.HyperLiquid.Spot;
+
+public class Symbol() : SymbolBase(), ISymbol
+{
+
+    public async Task GetSymbolsAsync()
+    {
+        if (GlobalData.ExchangeListName.TryGetValue(ExchangeBase.ExchangeOptions.ExchangeName, out Model.CryptoExchange? exchange))
+        {
+            try
+            {
+                using var client = new HyperLiquidRestClient();
+                using CryptoDatabase database = new();
+                database.Open();
+
+
+                // tickers for volumes... (need volume because of filtered kline and price tickers)
+                GlobalData.AddTextToLogTab($"Reading symbol and ticker information from {ExchangeBase.ExchangeOptions.ExchangeName}");
+                //LimitRate.WaitForFairWeight(1);
+                var tickerInfo = await client.SpotApi.ExchangeData.GetExchangeInfoAndTickersAsync();
+                if (!tickerInfo.Success)
+                    GlobalData.AddTextToLogTab($"error getting symbol ticker info {tickerInfo.Error}");
+                if (tickerInfo == null)
+                    throw new ExchangeException("No ticker data received");
+                SaveExchangeInfo(tickerInfo, "tickers.json");
+
+                // index volume
+                SortedList<string, decimal> volumeTicker = [];
+                if (tickerInfo.Data != null && tickerInfo.Data != null)
+                {
+                    foreach (var tickerData in tickerInfo.Data.Tickers)
+                    {
+                        if (tickerData.Symbol != null)
+                        {
+                            string symbolName = tickerData.Symbol.Replace("/", "");
+                            volumeTicker.Add(symbolName, tickerData.QuoteVolume);
+                        }
+                    }
+                }
+
+
+
+                GlobalData.AddTextToLogTab($"Reading symbol information from {ExchangeBase.ExchangeOptions.ExchangeName}");
+                //LimitRate.WaitForFairWeight(1);
+                var symbolInfo = tickerInfo;
+                //var symbolInfo = await client.SpotApi.ExchangeData.GetExchangeInfoAndTickersAsync() ?? throw new ExchangeException("No exchange data retrieved (1)");
+                if (!symbolInfo.Success)
+                    GlobalData.AddTextToLogTab("error getting symbol information " + symbolInfo.Error);
+                if (symbolInfo.Data == null)
+                    throw new ExchangeException("No exchange data retrieved (2)");
+                SaveExchangeInfo(symbolInfo, "symbols.json");
+
+
+                // Om achteraf de niet aangeboden munten te deactiveren
+                SortedList<string, CryptoSymbol> activeSymbols = [];
+
+
+                using (var transaction = database.BeginTransaction())
+                {
+                    List<CryptoSymbol> cache = [];
+                    try
+                    {
+                        foreach (var symbolData in symbolInfo.Data.ExchangeInfo.Symbols)
+                        {
+                            {
+                                if (symbolData.Name != symbolData.BaseAsset.Name + '/' + symbolData.QuoteAsset.Name)
+                                {
+                                    //GlobalData.AddTextToLogTab($"Ignoring symbol {symbolInfo.Name} {symbolInfo.BaseAsset} {symbolInfo.QuoteAsset} weird name?");
+                                    continue;
+                                }
+                                string symbolName = symbolData.BaseAsset.Name + symbolData.QuoteAsset.Name;
+
+                                //Eventueel symbol toevoegen
+                                if (!exchange.SymbolListName.TryGetValue(symbolName, out CryptoSymbol? symbol))
+                                {
+                                    var quoteData = GlobalData.AddQuoteData(symbolData.QuoteAsset.Name);
+
+                                    symbol = new()
+                                    {
+                                        Exchange = exchange,
+                                        ExchangeId = exchange.Id,
+                                        Name = symbolName,
+                                        Base = symbolData.BaseAsset.Name,
+                                        Quote = symbolData.QuoteAsset.Name,
+                                        QuoteData = quoteData,
+                                        Status = 1,
+                                    };
+                                }
+
+                                //Tijdelijk alles overnemen (vanwege into nieuwe velden)
+                                //De te gebruiken precisie in prijzen
+                                //symbol.BaseAssetPrecision = binanceSymbol.LotSizeFilter.BasePrecision.ToString().Length - 2;
+                                //if (symbol.BaseAssetPrecision <= 0)
+                                //    symbol.BaseAssetPrecision = 8;
+                                //symbol.QuoteAssetPrecision = binanceSymbol.LotSizeFilter.QuotePrecision.ToString().Length - 2;
+                                //if (symbol.QuoteAssetPrecision <= 0)
+                                //    symbol.QuoteAssetPrecision = 8;
+                                //symbol.MinNotional = binanceSymbol.MinNotional; // ????
+
+                                // min, max en tick (in base amount)
+                                //if (symbolData.BaseAsset.PriceDecimals)
+                                //    symbol.QuantityTickSize = symbolData.LotSize.Value;
+                                symbol.QuantityTickSize = symbolData.BaseAsset.QuantityDecimals;
+                                //symbol.QuantityMinimum = symbolInfo.LotSizeFilter?.MinOrderQuantity ?? 0;
+                                //symbol.QuantityMaximum = symbolInfo.LotSizeFilter?.MaxOrderQuantity ?? 0;
+
+                                //symbol.QuoteValueMinimum = symbolInfo.LotSizeFilter?.MinOrderValue ?? 0;
+                                //symbol.QuoteValueMaximum = symbolInfo.LotSizeFilter?.MaxOrderValue ?? 0;
+
+
+                                // De minimale en maximale prijs voor een order (in base price)
+                                // In de definities is wel een minPrice en maxprice aanwezig, maar die is niet gevuld
+                                // (dat heeft consequenties voro de werking van de Clamp die wel waarden verwacht)
+                                //symbol.PriceMinimum = symbolInfo.LotSizeFilter.MinOrderValue;
+                                //symbol.PriceMaximum = symbolInfo.LotSizeFilter.MaxOrderValue;
+
+                                symbol.PriceTickSize = symbolData.BaseAsset.PriceDecimals;
+
+                                symbol.IsSpotTradingAllowed = true; // binanceSymbol.IsSpotTradingAllowed;
+                                symbol.IsMarginTradingAllowed = false; // binanceSymbol.MarginTading; ???
+
+                                // volume from the tickers
+                                if (volumeTicker.TryGetValue(symbol.Name, out decimal volume))
+                                    symbol.Volume = volume;
+                                else
+                                    symbol.Volume = 0;
+
+                                //if (symbolData.BaseAsset.QuantityDecimalsState == InstrumentState.Live)
+                                    symbol.Status = 1;
+                                //else
+                                  //  symbol.Status = 0; //Zet de status door (PreTrading, PostTrading of Halt)
+
+                                if (symbol.Id == 0)
+                                {
+                                    database.Connection.Insert(symbol, transaction);
+                                    cache.Add(symbol);
+                                }
+                                else
+                                    database.Connection.Update(symbol, transaction);
+                                activeSymbols.Add(symbol.Name, symbol);
+                            }
+                        }
+
+                        // Deactiveer de munten die niet meer voorkomen
+                        int deactivated = 0;
+                        foreach (CryptoSymbol symbol in exchange.SymbolListName.Values)
+                        {
+                            if (symbol.Status == 1 && !symbol.IsBarometerSymbol() && !activeSymbols.ContainsKey(symbol.Name))
+                            {
+                                deactivated++;
+                                symbol.Status = 0;
+                                database.Connection.Update(symbol, transaction);
+                            }
+                        }
+                        if (deactivated > 0)
+                            GlobalData.AddTextToLogTab($"{deactivated} munten gedeactiveerd");
+
+                        transaction.Commit();
+
+
+                        // De nieuwe symbols toevoegen aan de lijst
+                        // (omdat de symbols pas tijdens de BulkInsert een id krijgen)
+                        foreach (CryptoSymbol symbol in cache)
+                        {
+                            GlobalData.AddSymbol(symbol);
+                        }
+
+                    }
+                    catch (Exception error)
+                    {
+                        ScannerLog.Logger.Error(error, "");
+                        GlobalData.AddTextToLogTab(error.ToString());
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+
+                exchange.LastTimeFetched = DateTime.UtcNow;
+                database.Connection.Update(exchange);
+
+            }
+            catch (Exception error)
+            {
+                ScannerLog.Logger.Error(error, "");
+                GlobalData.AddTextToLogTab(error.ToString());
+            }
+
+        }
+    }
+}
