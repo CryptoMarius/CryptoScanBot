d18396aa1c2c4d85727d652155d2324a2e6767ec
Marius <59998287+M22arius494@users.noreply.github.com>
2025-12-22 18:01:10 +0100

    Old InitializeCefRuntime()

--- CryptoScanner/Browser/Helpers/CustomCefApp.cs
+++ CryptoScanner/Browser/Helpers/CustomCefApp.cs
@@ -1,4 +1,8 @@
-﻿using Xilium.CefGlue;
+﻿using CryptoScanner.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Xilium.CefGlue;
 
 namespace CryptoScanner.Browser.Helpers;
 
@@ -8,7 +12,32 @@ public class CustomCefApp : CefApp
     {
         // Optional: Disable GPU for compatibility
         // commandLine.AppendSwitch("disable-gpu");
-
+        commandLine.AppendSwitch("disable-gpu");
+        commandLine.AppendSwitch("disable-gpu-compositing");
         base.OnBeforeCommandLineProcessing(processType, commandLine);
+    }
+
+
+    public static void InitializeCefRuntime()
+    {
+        //return;
+        System.Diagnostics.Debug.WriteLine($"InitializeCefRuntime");
+
+        //var platformService = App.Services.GetRequiredService<IPlatformService>();
+        //string dataPath = platformService.GetDataDirectory();
+
+        // Load CefGlue runtime
+        //CefRuntime.Load();
+
+        var settings = new CefSettings
+        {
+            //CachePath = Path.Combine(dataPath, "Browser"),
+            //LogFile = Path.Combine(dataPath, "Browser", "cef.log"),
+            LogSeverity = CefLogSeverity.Warning,
+            WindowlessRenderingEnabled = true,
+        };
+
+        var mainArgs = new CefMainArgs([]);
+        CefRuntime.Initialize(mainArgs, settings, new CustomCefApp(), IntPtr.Zero);
     }
 }
