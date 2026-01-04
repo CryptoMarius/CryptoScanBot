27b4acaaa3b1e6ba36a97ba8e5f15ee36850a626
Marius <59998287+M22arius494@users.noreply.github.com>
2026-01-04 18:38:52 +0100

    Added commands

--- CryptoScanner/Helpers/CommandHelper.cs
+++ CryptoScanner/Helpers/CommandHelper.cs
@@ -7,39 +7,42 @@ namespace CryptoScanner.Helpers;
 
 public static class CommandHelper
 {
-    public static void ActivateTradingApp(this CommandBase commandBase, CryptoTradingApp externalTradingApp, CryptoSymbol symbol, CryptoInterval interval, CryptoExternalUrlType viaTradingBrowser, bool activateTab = true)
+    public static void ActivateTradingApp(CryptoTradingApp tradingApp,
+        CryptoSymbol symbol, CryptoInterval interval, CryptoExternalUrlType viaTradingBrowser, bool activateTab = true)
     {
-        // Activate the trading application (and we use a dummy browser for AltradyStandard)
-
-        (string Url, CryptoExternalUrlType Execute) = GlobalData.ExternalUrls.GetExternalRef(externalTradingApp, false, symbol, interval);
+        // Activate the trading application (and we use a dummy browser for Altrady)
+        GlobalData.LoadLinkSettings(); // refresh links
+        (string Url, CryptoExternalUrlType Execute) = GlobalData.ExternalUrls.GetExternalRef(tradingApp, false, symbol, interval);
         if (Url != "")
         {
             GlobalData.AddTextToLogTab($"Linktools activate {Url}");
             //App.EventOpenInInternalBrowser?.Invoke(this, Url);
-            App.OpenInInternalBrowser(commandBase, Url);
 
-            //// Open the url via our own hidden browser (to avoid the AltradyStandard jump-step)
-            //if (viaTradingBrowser == CryptoExternalUrlType.Internal)
-            //{
-            //    //await WebViewTradingView.ActivateUrlAsync(Url);
-            //    //if (activateTab && TabControl != null)
-            //    //    TabControl.SelectedTab = TabPageBrowser;
-            //    // Usage anywhere:
-            //    App.HiddenBrowser.Navigate(Url);
-            //}
-            //else
-            //{
-            //    if (Execute == CryptoExternalUrlType.Internal)
-            //    {
-            //        // Send url-event via the MainWindowViewModel
-            //        EventOpenInInternalBrowser?.Invoke(this, Url);
-            //    }
-            //    else
-            //    {
-            //        // Open via the external (system) browser
-            //        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(Url) { UseShellExecute = true });
-            //    }
-            //}
+            // Open the url via our own hidden browser (to avoid the Altrady jump-step)
+            if (viaTradingBrowser == CryptoExternalUrlType.Internal)
+            {
+                //await WebViewTradingView.ActivateUrlAsync(Url);
+                //if (activateTab && TabControl != null)
+                //    TabControl.SelectedTab = TabPageBrowser;
+                // Usage anywhere:
+                //App.OpenInHiddenBrowser(Url);
+                App.OpenInInternalBrowser(null, Url);
+            }
+            else
+            {
+                if (Execute == CryptoExternalUrlType.Internal)
+                {
+                    // Send url-event via the MainWindowViewModel
+                    //EventOpenInInternalBrowser?.Invoke(this, Url);
+                    //App.OpenInInternalBrowser(commandBase, Url);
+                    App.OpenInHiddenBrowser(Url);
+                }
+                else
+                {
+                    // Open via the external (system) browser
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(Url) { UseShellExecute = true });
+                }
+            }
         }
     }
 
