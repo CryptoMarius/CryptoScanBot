97a5cb7ae0b74f03b0a60a1b02bd10555cc41665
Marius <59998287+M22arius494@users.noreply.github.com>
2026-01-10 16:04:24 +0100

    Price for bb lower and upper was not correct (rounded)

---
+++ CryptoScanner/Converters/BbLowerTextConverter.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Data.Converters;
+
+using CryptoScanner.ViewModels;
+
+namespace CryptoScanner.Converters;
+
+public class BbLowerTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        // Display the price using the symbol's price format
+        if (value is SignalViewModel signalInfo)
+        {
+            var price = signalInfo.BbLower;
+            return price?.ToString(signalInfo.Object.Symbol.PriceDisplayFormat);
+        }
+        else if (value is LiveDataViewModel liveDataInfo)
+        {
+            var price = liveDataInfo.BbLower;
+            return price?.ToString(liveDataInfo.Object.Symbol.PriceDisplayFormat);
+        }
+        else if (value is PositionViewModel positionInfo)
+        {
+            var price = positionInfo.BbLower;
+            return price?.ToString(positionInfo.Object.Symbol.PriceDisplayFormat);
+        }
+
+        return "?";
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        // ConvertBack not needed for OneWay binding
+        throw new NotImplementedException();
+    }
+}
---
+++ CryptoScanner/Converters/BbUpperTextConverter.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Data.Converters;
+
+using CryptoScanner.ViewModels;
+
+namespace CryptoScanner.Converters;
+
+public class BbUpperTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        // Display the price using the symbol's price format
+        if (value is SignalViewModel signalInfo)
+        {
+            var price = signalInfo.BbUpper;
+            return price?.ToString(signalInfo.Object.Symbol.PriceDisplayFormat);
+        }
+        else if (value is LiveDataViewModel liveDataInfo)
+        {
+            var price = liveDataInfo.BbUpper;
+            return price?.ToString(liveDataInfo.Object.Symbol.PriceDisplayFormat);
+        }
+        else if (value is PositionViewModel positionInfo)
+        {
+            var price = positionInfo.BbUpper;
+            return price?.ToString(positionInfo.Object.Symbol.PriceDisplayFormat);
+        }
+
+        return "?";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        // ConvertBack not needed for OneWay binding
+        throw new NotImplementedException();
+    }
+}
