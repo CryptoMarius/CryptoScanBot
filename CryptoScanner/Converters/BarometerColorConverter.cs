fa5706f6f5646095a7351847b226fa43b6792994
Marius <59998287+M22arius494@users.noreply.github.com>
2026-01-30 13:42:28 +0100

    Cleanup converters

--- CryptoScanner/Converters/BarometerColorConverter.cs
+++ CryptoScanner/Converters/BarometerColorConverter.cs
@@ -1,9 +1,20 @@
 ﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+using System.Globalization;
 
 namespace CryptoScanner.Converters;
 
-public class BarometerColorConverter : ColorConverter, IValueConverter
+public class BarometerColorConverter : IValueConverter
 {
+    internal static IBrush? GetBrushResource(string key) => App.GetBrushResource(key);
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // ConvertBack not needed for OneWay binding
+        throw new NotImplementedException();
+    }
+
     public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
     {
         if (value is decimal valueDecimal)
