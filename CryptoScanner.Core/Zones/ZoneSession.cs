1e798d02d02c48366f43fb66907c59a1a029c081
Marius <59998287+M22arius494@users.noreply.github.com>
2026-03-26 08:58:34 +0100

    Added bbma lines

--- CryptoScanner.Core/Zones/ZoneSession.cs
+++ CryptoScanner.Core/Zones/ZoneSession.cs
@@ -44,10 +44,12 @@ public class ZoneSession
     public bool ShowFvgZones { get; set; } = false; // Fear Value Gaps
     public bool ShowDtb { get; set; } = false; // Double Top Double Bottom
     public bool ShowNadarayaWatsonEnvelope { get; set; } = true; // NWE non repainting?
-    public bool ShowNadarayaWatsonEnvelopeRepainting { get; set; } = true;
+    public bool ShowNadarayaWatsonEnvelopeRepainting { get; set; } = false;
     public bool ShowPSar { get; set; } = false;
-    public bool ShowBollingerBand { get; set; } = false;
-    public bool ShowSmaLinesSbm { get; set; } = true;
+    public bool ShowBollingerBand { get; set; } = true;
+    public bool ShowSmaLinesSbm { get; set; } = false;
+    public bool ShowBbma { get; set; } = false;
+    
 
     // options
     public bool ShowPoints { get; set; } = false; // Pivot points (debug)
