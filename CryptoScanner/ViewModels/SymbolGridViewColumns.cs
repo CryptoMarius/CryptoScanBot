74ef2bcc0bb6e667a1c4a5d03ba4f8768ef6e023
Marius <59998287+M22arius494@users.noreply.github.com>
2026-04-03 10:38:44 +0200

    Fix sort on volume

--- CryptoScanner/ViewModels/SymbolGridViewColumns.cs
+++ CryptoScanner/ViewModels/SymbolGridViewColumns.cs
@@ -41,7 +41,7 @@ public class SymbolColumnComparer : IComparer
                 {
                     SymbolColumnEnum.Id => ObjectCompare.Compare(a.Id, b.Id),
                     SymbolColumnEnum.Symbol => ObjectCompare.Compare(a.Symbol, b.Symbol),
-                    SymbolColumnEnum.Volume => ObjectCompare.Compare(a.Volume, b.Volume),
+                    SymbolColumnEnum.Volume => ObjectCompare.Compare(a.Object.Volume, b.Object.Volume),
                     //SymbolColumnEnum.Price => ObjectCompare.Compare(a.LastPrice, b.LastPrice),
                     SymbolColumnEnum.Distance => ObjectCompare.Compare(ZoneTools.ZoneDistance(a.Object), ZoneTools.ZoneDistance(b.Object)),
                     //SymbolColumnEnum.MarketTrendPrimary => ObjectCompare.Compare(MarketTrendPrimary(a), MarketTrendPrimary(b)),
